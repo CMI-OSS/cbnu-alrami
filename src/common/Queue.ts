3bb572d9bc998058caea29c90919e7d4c00b593f
jess <jaryapp@gmail.com>
2021-10-10 01:08:53 +0900

    feat: Add Queue

---
+++ src/common/Queue.ts
@@ -0,0 +1,54 @@
+interface QueueNode<T> {
+  next: QueueNode<T> | null;
+  data: T;
+}
+
+class Queue<T> {
+  head: QueueNode<T> | null = null;
+  tail: QueueNode<T> | null = null;
+  length = 0;
+
+  front() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    return this.head;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  push(data: T) {
+    if (this.head === null || this.tail === null) {
+      this.head = {
+        data,
+        next: null,
+      };
+      this.tail = this.head;
+      this.length += 1;
+      return;
+    }
+
+    this.tail.next = {
+      data,
+      next: null,
+    };
+
+    this.tail = this.tail.next;
+
+    this.length += 1;
+  }
+
+  pop() {
+    if (this.isEmpty() || this.head === null) {
+      return null;
+    }
+    var node = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+
+    return node.data;
+  }
+}

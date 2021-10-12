a1d2d441a716ad4d40a1c34b688fc201c6682083
동동이 <drdragon777@naver.com>
2021-10-13 02:00:31 +0900

    feat: Create ArrayToDate module

---
+++ scrapper-server/src/scrappers/CalendarScrapper/ArrayToDate.ts
@@ -0,0 +1,39 @@
+export default function ArrayToDate(arr: string) {
+  const validDate = (i: number) => {
+    return arr[i].length === 1 ? 0 + arr[i] : arr[i];
+  };
+  switch (arr.length) {
+    case 2:
+      return {
+        start_date: `2021-${validDate(0)}-${validDate(1)}`,
+        end_date: null,
+      };
+    case 3:
+      return {
+        start_date: `2021-${validDate(0)}-${validDate(1)}`,
+        end_date: `2021-${validDate(0)}-${validDate(2)}`,
+      };
+    case 4:
+      return {
+        start_date:
+          arr[0].length === 4
+            ? `${arr[0]}-${validDate(1)}-${validDate(2)}`
+            : `2021-${validDate(0)}-${validDate(1)}`,
+        end_date:
+          arr[0].length === 4
+            ? `${arr[0]}-${validDate(1)}-${validDate(3)}`
+            : `2021-${validDate(2)}-${validDate(3)}`,
+      };
+    case 5:
+      return {
+        start_date:
+          arr[0].length === 4
+            ? `${arr[0]}-${validDate(1)}-${validDate(2)}`
+            : `2021-${validDate(0)}-${validDate(1)}`,
+        end_date:
+          arr[0].length === 4
+            ? `${arr[0]}-${validDate(3)}-${validDate(4)}`
+            : `${arr[2]}-${validDate(3)}-${validDate(4)}`,
+      };
+  }
+}

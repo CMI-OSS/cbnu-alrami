0d127ab98f7e3ee70b0b0b0148920a14401a4a64
jaryapp <jaryapp@gmail.com>
2021-11-04 04:27:52 +0900

    setting: Add craco for absolute path

---
+++ craco.alias.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+module.exports = {
+  "@admin": path.resolve(__dirname, "packages/admin/src/"),
+  "@scraper": path.resolve(__dirname, "packages/scraper/src/"),
+  "@server": path.resolve(__dirname, "packages/server/src/"),
+};
---
+++ packages/admin/craco.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const alias = require("../../craco.alias");
+
+module.exports = {
+  webpack: {
+    alias,
+  },
+};

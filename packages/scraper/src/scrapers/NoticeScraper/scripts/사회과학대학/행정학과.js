2a44edc474675cef2b8632dce7717fa12e1541f2
jaryapp <jaryapp@gmail.com>
2021-11-10 01:59:58 +0900

    feat: Add "사회과학대학" scripts

---
+++ packages/scraper/src/scrapers/NoticeScraper/scripts/사회과학대학/행정학과.js
@@ -0,0 +1,10 @@
+const 경제학과 = require("./경제학과");
+
+const script = {
+  url: "https://politics.chungbuk.ac.kr/board/department_notice",
+  site_id: 50501,
+  site: "행정학과",
+  category: "학부/융합전공공지",
+};
+
+module.exports = { ...경제학과, ...script };

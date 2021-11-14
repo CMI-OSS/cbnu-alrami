04d5905739d967f731e5a709cb4ccff81f0ff97f
jaryapp <jaryapp@gmail.com>
2021-11-14 17:49:13 +0900

    feat: Add "자연과학대학' scripts

---
+++ packages/scraper/src/scrapers/NoticeScraper/scripts/자연과학대학/수학과.js
@@ -0,0 +1,10 @@
+const 물리학과 = require("./물리학과");
+
+const script = {
+  url: "http://math.chungbuk.ac.kr/?pg_idx=145",
+  site_id: 120501,
+  site: "수학과",
+  category: "공지사항",
+};
+
+module.exports = { ...물리학과, script };
---
+++ packages/scraper/src/scrapers/NoticeScraper/scripts/자연과학대학/지구환경과학과.js
@@ -0,0 +1,10 @@
+const 물리학과 = require("./물리학과");
+
+const script = {
+  url: "http://geology.chungbuk.ac.kr/?pg_idx=145",
+  site_id: 120701,
+  site: "지구환경과학과",
+  category: "공지사항",
+};
+
+module.exports = { ...물리학과, script };

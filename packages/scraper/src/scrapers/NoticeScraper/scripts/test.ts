e3c21e1aab11eff28eda1d34fcefe23d78c85b9e
jaryapp <jaryapp@gmail.com>
2021-11-15 01:01:15 +0900

    feat: Add notice list test

--- packages/scraper/src/scrapers/NoticeScraper/scripts/test.ts
+++ packages/scraper/src/scrapers/NoticeScraper/scripts/test.ts
@@ -1,20 +1,26 @@
 import find from "find";
+import NoticeScraper from "../index";
 import { NoticeScript } from "@scraper/interfaces";
+import { Scenario } from "../../Scenario";
+
+function loadScripts(scriptPath: string): Promise<Array<NoticeScript>> {
+  return new Promise((resolve, _) => {
+    const scripts: Array<NoticeScript> = [];
+    find.file(/\.js$/, scriptPath, (paths: string[]) => {
+      for (const path of paths) {
+        const script = require(path);
+        scripts.push(script);
+      }
+      resolve(scripts);
+    });
+  });
+}
+
+function wait(milliseconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
 
 async function checkOverlapped() {
-  function loadScripts(scriptPath: string): Promise<Array<NoticeScript>> {
-    return new Promise((resolve, _) => {
-      const scripts: Array<NoticeScript> = [];
-      find.file(/\.js$/, scriptPath, (paths: string[]) => {
-        for (const path of paths) {
-          const script = require(path);
-          scripts.push(script);
-        }
-        resolve(scripts);
-      });
-    });
-  }
-
   const scripts = await loadScripts(__dirname);
 
   for (const target of scripts) {
@@ -34,4 +40,28 @@ async function checkOverlapped() {
   console.log("No Similar scripts");
 }
 
+async function checkNoticeList() {
+  await NoticeScraper.initScraper();
+  NoticeScraper.pause();
+
+  const scripts = await loadScripts(__dirname);
+
+  for (const target of scripts) {
+    try {
+      const scenario = new Scenario(target);
+      const notices = await NoticeScraper.getNoticeList(scenario);
+      if (notices.length === 0) {
+        throw Error("공지사항 리스트 없음");
+      }
+    } catch (error) {
+      console.error(error);
+      NoticeScraper.stop();
+      throw Error(`[${target.site}] 공지사항 리스트 스크래핑 fail`);
+    }
+
+    await wait(1000);
+  }
+}
+
 checkOverlapped();
+checkNoticeList();

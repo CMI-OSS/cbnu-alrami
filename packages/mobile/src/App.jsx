ff9da92b00fc3b3699e5e7fc39d0c9e4ae1c4d9d
nohgijin <ynda11111@naver.com>
2021-11-29 17:38:24 +0900

    setting: mobile 기본 환경 설정 - cra없이 기본으로 설정

---
+++ packages/mobile/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return <div className="container" />;
+};
+
+export default App;
---
+++ packages/mobile/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devtool: "eval-cheap-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jfif$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};

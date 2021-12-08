35cadfad610f6e2b4aa33a03af32a482b80c979d
제스 <49256790+jaryapp@users.noreply.github.com>
2021-12-08 18:30:50 +0900

    [scraper] 스크래퍼 린트 적용 (#69)
    * :fire: Remove .DS_Store
    
    * :fire: Remove yarn-error.log
    
    * :see_no_evil: Add gitignore
    
    * ✨ 스크래퍼 린트 적용

--- packages/scraper/src/common/store.ts
+++ packages/scraper/src/common/store.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "../reducers";
 import thunk from "redux-thunk";
+import rootReducer from "../reducers";
 
 const store = createStore(
   rootReducer,
--- packages/scraper/src/interfaces/Calendar.ts
+++ packages/scraper/src/interfaces/Calendar.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 export interface Calendar {
   start_date: string;
   end_date: string | null;

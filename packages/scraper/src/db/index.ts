118a3fa5b09d7107541daf1b6bec23efa89ee0b8
jaryapp <jaryapp@gmail.com>
2021-12-11 18:56:04 +0900

    ✨ Change mysql pool and promise

--- packages/scraper/src/db/index.ts
+++ packages/scraper/src/db/index.ts
@@ -1,12 +1,12 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
 });
 
-export default connection;
+export default pool.promise();

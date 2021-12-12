030f7f3b1a728ed3ae20287381121ac86d48b84f
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-12-12 20:56:46 +0900

    chore: 패키지 설치

--- packages/admin/webpack.config.js
+++ packages/admin/webpack.config.js
@@ -3,6 +3,8 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   devServer: {
@@ -15,7 +17,8 @@ module.exports = {
     main: "./src/index.tsx",
   },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
+    chunkFilename: '[name].[hash].bundle.js',
     path: path.resolve(__dirname, "./build"),
   },
   devtool: "eval-cheap-source-map",
@@ -36,6 +39,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        exclude: "/node_modules/",
         use: [ "style-loader", "css-loader" ],
       },
       {
@@ -47,11 +51,21 @@ module.exports = {
       },
     ],
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  optimization: {
+    minimizer: 
+     [ new OptimizeCSSAssetsPlugin() ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin()
   ],
 };

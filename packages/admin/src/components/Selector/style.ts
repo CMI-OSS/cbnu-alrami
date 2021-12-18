6b5db5dde491bbd26e303bf08985085eb389b345
Hyuno Choi <soonitoon@gmail.com>
2021-12-18 16:58:27 +0900

    style: Add google font and style for select tag

--- packages/admin/src/components/Selector/style.ts
+++ packages/admin/src/components/Selector/style.ts
@@ -1,21 +1,27 @@
 import { css } from "@emotion/css";
+import { colors } from "@shared/styles/color";
 import { hashClassNames } from "src/utils/hash";
 
 export default () => {
-  const hashedStyle = hashClassNames([ "selector" ]);
-  const { selector } = hashedStyle;
+  const hashedStyle = hashClassNames([ "select", "label", "arrowIcon" ]);
+  const { label, select, arrowIcon } = hashedStyle;
 
-  const selectorContainer = css`
+  const selectContainer = css`
     display: flex;
     margin-left: 3rem;
 
-    .${selector} {
+    .${select} {
       height: 2rem;
       padding: 0.5rem;
       margin: 0 1rem;
       border: none;
+      border-radius: 0.7rem;
+      :hover {
+        box-shadow: 0 0 0 0.1rem ${colors.$lightBlue};
+        cursor: pointer;
+      }
     }
   `;
 
-  return { selectorContainer, selector };
+  return { selectContainer, select, label, arrowIcon };
 };

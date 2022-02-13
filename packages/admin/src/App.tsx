dcdfc844678a2ac9c6b69b4e4fe1e881426daeab
동동이 <drdragon777@naver.com>
2022-02-14 00:03:30 +0900

    feat: Refactoring Navigation bar

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { hot } from "react-hot-loader";
 import getGlobalStyle from "@shared/styles/globalStyle";
 import getAdminStyle from "src/adminStyle";
+import Navigation from "./components/Navigation";
 import LoginPage from "./pages/Login";
 import { store } from "./store";
 
@@ -14,10 +15,11 @@ function App() {
       <Global styles={getGlobalStyle()} />
       <Global styles={getAdminStyle()} />
       <BrowserRouter>
+        <Navigation />
         <Routes>
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/scraper/*" element={<ScraperPage/>}/>
-          <Route path="*" element={<Navigate to="/scraper/notice" />}/>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/scraper/*" element={<ScraperPage />} />
+          <Route path="*" element={<Navigate to="/scraper/notice" />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- packages/admin/src/components/Navigation/index.tsx
+++ packages/admin/src/components/Navigation/index.tsx
@@ -1,9 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { cx } from "@emotion/css";
+import { useState } from "react";
+import { NavLink, useMatch } from "react-router-dom";
 import getStyle from "./style";
 
 export default function Natigation() {
+  const isLoginMatch = useMatch("/login");
+  const [ isBoardActive, setBoardActive ] = useState(false);
+  const [ isScraperActive, setScraperActive ] = useState(false);
   const style = getStyle();
-  const MENUS = [
+  const SCRAPER_MENUS = [
     {
       path: "/scraper/notice",
       label: "공지사항",
@@ -22,20 +27,64 @@ export default function Natigation() {
     },
   ];
 
-  return (
-    <nav className={style.Navigation}>
-      <ul className={style.sideNavUl}>
-        <li className={style.logo}>CMI</li>
-        {MENUS.map(({ path, label }) => (
-          <NavLink
-            key={path}
-            to={path}
-            className={({isActive}) => style.sideNavLi + (isActive? ` ${style.activated}` : "")}
-          >
-            {label}
-          </NavLink>
-        ))}
-      </ul>
-    </nav>
+  const BOARD_MENUS = [
+    {
+      path: "/board/list",
+      label: "게시물 목록",
+    },
+    {
+      path: "/board/write",
+      label: "게시물 작성",
+    },
+  ];
+
+  return isLoginMatch ? (
+    <></>
+  ) : (
+    <>
+      <nav className={style.Navigation}>
+        <ul className={style.sideNavUl}>
+          <li className={style.logo}>CMI</li>
+          <ul>
+            {[ BOARD_MENUS, SCRAPER_MENUS ].map((menu, idx) => (
+              <li className={style.contentNavLi}>
+                <p className={style.detailLogo}>
+                  {!idx ? "게시판" : "스크래퍼"}
+                </p>
+                <ul
+                  className={cx(style.detailNavUl, {
+                    [style.contentsActivated]: !idx
+                      ? isBoardActive
+                      : isScraperActive,
+                  })}
+                >
+                  {menu.map(({ path, label }) => (
+                    <NavLink
+                      key={path}
+                      to={path}
+                      className={({ isActive }) => {
+                        if (!idx) {
+                          setBoardActive(true);
+                          setScraperActive(false);
+                        } else {
+                          setBoardActive(false);
+                          setScraperActive(true);
+                        }
+                        return (
+                          style.sideNavLi +
+                          (isActive ? ` ${style.activated}` : "")
+                        );
+                      }}
+                    >
+                      {label}
+                    </NavLink>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </ul>
+      </nav>
+    </>
   );
 }

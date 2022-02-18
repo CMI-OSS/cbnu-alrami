5e9dfd724f1943d78c0ad86ca2bc06ff18625914
jaryapp <jaryapp@gmail.com>
2022-02-19 02:59:10 +0900

    ✨ Add handleStartScraper

--- packages/scraper/src/index.ts
+++ packages/scraper/src/index.ts
@@ -1,8 +1,37 @@
 import "src/common/env";
 import NoticeScraper from "src/scrapers/NoticeScraper";
+import CalendarScrpaer from "src/scrapers/CalendarScraper";
+import CafeteriaScraper from "src/scrapers/CafeteriaScraper";
+import DomitoryScraper from "src/scrapers/DomitoryScraper";
 
 import { Server, Socket } from "socket.io";
 import { ScraperType } from "@shared/types";
 import { SocketMessageType } from "./types";
 
 const io = new Server(Number(process.env.SOCKET_SERVER_PORT) ?? 3000);
+
+const onSocketConnection = (socket: Socket) => {
+  socket.on(SocketMessageType.START_SCRAPER, handleStartScraper);
+};
+
+const handleStartScraper = ({ scraperType }: { scraperType: ScraperType }) => {
+  switch (scraperType) {
+    case "notice":
+      NoticeScraper.start();
+      break;
+    case "collegeSchedule":
+      CalendarScrpaer.start();
+      break;
+    case "domitoryCafeteria":
+      DomitoryScraper.start();
+      break;
+    case "studentCafeteria":
+      CafeteriaScraper.start();
+      break;
+    default:
+      throw new Error(`Not found "${scraperType}" scraper `);
+      break;
+  }
+};
+
+io.on("connection", onSocketConnection);
--- packages/scraper/src/types/index.ts
+++ packages/scraper/src/types/index.ts
@@ -4,3 +4,4 @@ export type { NoticeScript } from "./NoticeScript";
 export type { Calendar } from "./Calendar";
 export type { CalendarScript } from "./CalendarScript";
 export type { CafeteriaScript } from "./CafeteriaScript";
+export { SocketMessageType } from "./Socket";

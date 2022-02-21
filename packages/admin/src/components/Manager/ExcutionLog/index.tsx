dd113c1ea9040e11797244ff4f78795fbfca6c60
jaryapp <jaryapp@gmail.com>
2022-02-21 16:49:14 +0900

    ✨ LogBox scroll move end when new log receive

--- packages/admin/src/components/Manager/ExcutionLog/index.tsx
+++ packages/admin/src/components/Manager/ExcutionLog/index.tsx
@@ -1,4 +1,5 @@
 import { ScraperLog } from "@shared/types";
+import { useEffect, useRef } from "react";
 import getStyle from "./style";
 
 interface Props {
@@ -7,11 +8,16 @@ interface Props {
 
 export default function ExcutionLog({ logs }: Props) {
   const style = getStyle();
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    boxRef.current?.scrollIntoView(false);
+  }, [ logs ]);
 
   return (
     <section className={style.excutionLog}>
       <h2 className={style.excutionLogTitle}>실행 로그</h2>
-      <div className={style.excutionBox}>
+      <div className={style.excutionBox} ref={boxRef}>
         {logs.map((log, index) => (
           // eslint-disable-next-line react/no-array-index-key
           <p key={index} className={style.log}>

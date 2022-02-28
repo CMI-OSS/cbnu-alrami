a796cc57f2a73bccdbbfa44e23839aefd5b1d312
vcho1958 <gusckd57@gmail.com>
2022-02-28 14:20:03 +0900

    feat: add server package made with nestjs

--- packages/server/src/socket_handlers/sample.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from "socket.io";
-
-const sampleHandler = (socket: Socket) => {
-  socket.on("time", () => {
-    socket.emit("serverTime", new Date().getTime());
-  });
-};
-
-export default sampleHandler;

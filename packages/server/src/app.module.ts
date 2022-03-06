466b864ee35e28e1c8b79dccc7a77d3d32411653
vcho1958 <gusckd57@gmail.com>
2022-03-06 09:28:18 +0900

    feat: add configure module as dynamic for register to DatabaseModule

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from './config/config.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.register(process.env.NODE_ENV)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import getConfiguration from './config/configuration';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(getConfiguration(process.env.NODE_ENV).http.port);
 }
 bootstrap();

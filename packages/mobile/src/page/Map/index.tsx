c3811c5a96f2134e78e29f42478a2cd69cd7bb54
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2022-03-25 20:48:55 +0900

    style: arrow 위치 수정

--- packages/mobile/src/page/Map/index.tsx
+++ packages/mobile/src/page/Map/index.tsx
@@ -2,6 +2,7 @@ import $ from "./style.module.scss";
 import MenuButtonList from "../../components/molecules/MenuButtonList";
 import { PlaceMenu } from "../../components/atoms/icon/PlaceMenu";
 import { SmallPlaceMenu } from "../../components/atoms/icon/SmallPlaceMenu";
+import { PlaceArrow } from "../../components/atoms/icon/PlaceArrow";
 
 function Map() {
   return (
@@ -14,9 +15,11 @@ function Map() {
             <br />
             다양한 장소를 탐색해요
           </span>
+          <PlaceArrow className={$.arrow} />
         </div>
         <a href="." className={$.link}>
           <PlaceMenu className={$.icon} />
+          <span className="blind">장소탐색하기</span>
         </a>
       </div>
     </>

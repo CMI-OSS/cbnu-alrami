271b3ad3bb6dfad82d8f1cb72bb3f494f95eb8bd
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2022-03-25 20:13:27 +0900

    style: 기본 템플릿 position:relative 설정

--- packages/mobile/src/page/Map/index.tsx
+++ packages/mobile/src/page/Map/index.tsx
@@ -1,7 +1,26 @@
 import $ from "./style.module.scss";
+import MenuButtonList from "../../components/molecules/MenuButtonList";
+import { PlaceMenu } from "../../components/atoms/icon/PlaceMenu";
+import { SmallPlaceMenu } from "../../components/atoms/icon/SmallPlaceMenu";
 
 function Map() {
-  return <section className={$.map}>Map</section>;
+  return (
+    <>
+      <MenuButtonList />
+      <div className={$.wrap}>
+        <div className={$.place_wrap}>
+          <span className={$.text}>
+            <SmallPlaceMenu className={$.small_icon} />를 눌러
+            <br />
+            다양한 장소를 탐색해요
+          </span>
+        </div>
+        <a href="." className={$.link}>
+          <PlaceMenu className={$.icon} />
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Map;

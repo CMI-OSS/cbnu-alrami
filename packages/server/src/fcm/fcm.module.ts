ef9ba5ae32d48a3aa00b2ecac47c5a456eb1f808
steve <ionc635@naver.com>
2022-03-27 19:48:09 +0900

    style: Prettier 추가 적용
    - fcm.module.ts

--- packages/server/src/fcm/fcm.module.ts
+++ packages/server/src/fcm/fcm.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
-import { FcmService } from './fcm.service'
+import { FcmService } from "./fcm.service";
 
 @Module({
   providers: [FcmService],
-  exports: [FcmService]
+  exports: [FcmService],
 })
 export class FcmModule {}

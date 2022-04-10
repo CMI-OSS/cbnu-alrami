073e09b85ddfed83a2b94fd1ff092672f572caa0
vcho1958 <gusckd57@gmail.com>
2022-04-10 17:32:17 +0900

    feat: jwt 인증 키 추가

---
+++ packages/server/src/@constants/constants.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import configuration from "src/config/configuration";
+
+const yaml = configuration();
+export const ROOT_DIR = join(__dirname, '../../');
+// export const ACCESS_PUBLIC_KEY = readFileSync(join(ROOT_DIR, yaml.accessPublicKeyFile));
+export const ACCESS_PRIVATE_KEY = readFileSync(join(ROOT_DIR, yaml.accessPrivateKeyFile));
+// export const REFRESH_PUBLIC_KEY = readFileSync(join(ROOT_DIR, yaml.refreshPublicKeyFile));
+// export const REFRESH_PRIVATE_KEY = readFileSync(join(ROOT_DIR, yaml.refreshPrivateKeyFile));

0359f7e6c2b3e1b08fd3c70c95c179cf621c18a6
EunjiShin <eunji980310@gmail.com>
2022-04-10 10:58:39 +0900

    🔥 typeORM csrf 설정 수정

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import helmet from "helmet";
-import * as csurf from "csurf";
 import * as compression from "compression";
 import { AppModule } from "./app.module";
 import getConfiguration from "./config/configuration";
@@ -9,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
-  app.use(csurf());
   app.use(compression());
   await app.listen(getConfiguration().http.port);
 

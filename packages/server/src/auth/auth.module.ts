fb23739c555c188e067ac0aaf01019f32a52d9c2
vcho1958 <gusckd57@gmail.com>
2022-04-10 18:04:55 +0900

    chore: AuthModule 불필요 import문 제거

--- packages/server/src/auth/auth.module.ts
+++ packages/server/src/auth/auth.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ACCESS_PRIVATE_KEY } from 'src/@constants/constants';
-import { JwtGuard } from 'src/@guard/jwt.guard';
-import { LocalGuard } from 'src/@guard/local.guard';
 import { JwtStrategy } from 'src/@strategy/jwt.strategy';
 import { LocalStrategy } from 'src/@strategy/local.strategy';
 import { AdminModule } from 'src/admin/admin.module';

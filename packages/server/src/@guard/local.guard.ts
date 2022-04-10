f4fc97fd3d0c4298c08449b90fc3f331179a64a0
vcho1958 <gusckd57@gmail.com>
2022-04-10 17:41:00 +0900

    feat: 로그인을 위한 LocalGuard 및 Strategy 생성

--- packages/server/src/@guard/local.guard.ts
+++ packages/server/src/@guard/local.guard.ts
@@ -1,11 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
-}
+export class LocalGuard extends AuthGuard('local') {}

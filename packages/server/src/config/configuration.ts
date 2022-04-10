31bf6083d20637acb3e5cf0bd404c8b4567f83d9
vcho1958 <gusckd57@gmail.com>
2022-04-10 18:41:44 +0900

    feat: 환경변수 yaml이외 기존 선언된 시스템 변수도 사용가능하게 변경

--- packages/server/src/config/configuration.ts
+++ packages/server/src/config/configuration.ts
@@ -5,7 +5,8 @@ import { join } from 'path';
 
 export default () => {
   const YAML_CONFIG_FILENAME = `../../${process.env.NODE_ENV ? process.env.NODE_ENV : 'prod'}.yaml`;
-  return yaml.load(
+  const configure = yaml.load(
     readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
   ) as Record<string, any>;
+  return {...configure, ...process.env};
 };

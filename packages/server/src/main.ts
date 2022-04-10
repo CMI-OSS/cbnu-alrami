b47697291594a0c0e5485bd0b404dc36a03f1d2b
vcho1958 <gusckd57@gmail.com>
2022-04-10 17:51:54 +0900

    fix: nested object 파싱 오류 해결

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from "@nestjs/core";
-import helmet from "helmet";
-import * as csurf from "csurf";
-import * as compression from "compression";
+import * as express from 'express';
+import helmet from 'helmet';
 import { AppModule } from "./app.module";
 import getConfiguration from "./config/configuration";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   app.use(helmet());
-  app.use(csurf());
-  app.use(compression());
+  app.enableCors();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   await app.listen(getConfiguration().http.port);
 
   // console.log(getConfiguration());   //환경변수 로딩 확인 코드

6a02e9259801e4440a43f130a1a097576a8062e8
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2022-04-17 00:24:40 +0900

    style: list 컴포넌트 마크업 추가

--- packages/mobile/src/page/Category/index.tsx
+++ packages/mobile/src/page/Category/index.tsx
@@ -9,8 +9,48 @@ const menuList = [
   { id: 2, name: "식사", path: "/food" },
 ];
 
+const hashList = [
+  { id: 1, name: "#학교", path: "/school_hash" },
+  { id: 2, name: "#N구역", path: "/area" },
+  { id: 3, name: "#8인_테이블", path: "/table" },
+];
+
+const imageList = [
+  {
+    id: 1,
+    name: "충북대학교 사회과학대학",
+    description: "충청북도 청주시 흥덕구 사창동 470",
+    src: "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180130_263%2F1517301583613dwuaL_JPEG%2FZh5SeEjUT12rWxcLJ2nstPaB.jpg",
+  },
+  {
+    id: 2,
+    name: "충북대학교 사회과학대학",
+    description: "충청북도 청주시 흥덕구 사창동 470",
+    src: "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180130_263%2F1517301583613dwuaL_JPEG%2FZh5SeEjUT12rWxcLJ2nstPaB.jpg",
+  },
+  {
+    id: 3,
+    name: "충북대학교 사회과학대학",
+    description: "충청북도 청주시 흥덕구 사창동 470",
+    src: "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180130_263%2F1517301583613dwuaL_JPEG%2FZh5SeEjUT12rWxcLJ2nstPaB.jpg",
+  },
+  {
+    id: 4,
+    name: "충북대학교 사회과학대학",
+    description: "충청북도 청주시 흥덕구 사창동 470",
+    src: "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180130_263%2F1517301583613dwuaL_JPEG%2FZh5SeEjUT12rWxcLJ2nstPaB.jpg",
+  },
+  {
+    id: 5,
+    name: "충북대학교 사회과학대학",
+    description: "충청북도 청주시 흥덕구 사창동 470",
+    src: "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180130_263%2F1517301583613dwuaL_JPEG%2FZh5SeEjUT12rWxcLJ2nstPaB.jpg",
+  },
+];
+
 function Category() {
   const [menu, setMenu] = useState(1);
+  const [hash, setHash] = useState(1);
 
   return (
     <>
@@ -48,6 +88,42 @@ function Category() {
             })}
           </div>
         </Flicking>
+      </div>
+      <div className={$.content}>
+        <div className={$.hash}>
+          {hashList.map((item, idx) => {
+            return (
+              <NavLink
+                key={`menu-${item.id}`}
+                to={item.path}
+                className={$.hash_link}
+                onClick={() => setHash(idx)}
+                aria-selected={hash === idx + 1}
+              >
+                {item.name}
+              </NavLink>
+            );
+          })}
+        </div>
+        <div className={$.image_list}>
+          {imageList.map((item, idx) => {
+            return (
+              <NavLink to="./" className={$.item} key={`menu-${idx}`}>
+                <img
+                  className={$.school_image}
+                  src={item.src}
+                  alt={item.name}
+                />
+                <div className={$.summary}>
+                  <strong className={$.summary_title}>{item.name}</strong>
+                  <span className={$.summary_description}>
+                    {item.description}
+                  </span>
+                </div>
+              </NavLink>
+            );
+          })}
+        </div>
       </div>
     </>
   );

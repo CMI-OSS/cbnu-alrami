10074f8a32fbf04d4ed3d2ea3af15f1bdf1cac51
steve <ionc635@naver.com>
2022-04-18 22:00:39 +0900

    chore: 디렉토리 명 변경
    - strategys -> strategies

--- packages/server/src/auth/auth.module.ts
+++ packages/server/src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { ACCESS_PRIVATE_KEY } from "src/commons/constants/constants";
-import { JwtStrategy } from "src/commons/strategys/jwt.strategy";
-import { LocalStrategy } from "src/commons/strategys/local.strategy";
+import { JwtStrategy } from "src/commons/strategies/jwt.strategy";
+import { LocalStrategy } from "src/commons/strategies/local.strategy";
 import { AdminModule } from "src/admin/admin.module";
 import { UserModule } from "src/user/user.module";
 import { AuthController } from "./auth.controller";

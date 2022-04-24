c97a5746028a18df35dcae4518c3bc117f1e715b
vcho1958 <gusckd57@gmail.com>
2022-04-24 19:39:16 +0900

    feat: snake naming strategy 적용

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
 import { AuthModule } from "./auth/auth.module";
 import { AwsModule } from "./aws/aws.module";
 import { BoardModule } from "./board/board.module";
+import { CafeteriaModule } from "./cafeteria/cafeteria.module";
 import configuration from "./commons/config/configuration";
 import { JwtGuard } from "./commons/guards/jwt.guard";
 import { FcmModule } from "./fcm/fcm.module";
+import { PlaceModule } from './place/place.module';
 
 @Module({
   imports: [
@@ -21,6 +24,7 @@ import { FcmModule } from "./fcm/fcm.module";
       useFactory: async (config: ConfigService) => ({
         ...config.get("db"),
         entities: [ `${__dirname}/commons/entities/*.js` ],
+        namingStrategy: new SnakeNamingStrategy(),
       }),
       inject: [ ConfigService ],
     }),
@@ -28,6 +32,8 @@ import { FcmModule } from "./fcm/fcm.module";
     AuthModule,
     AwsModule,
     BoardModule,
+    CafeteriaModule,
+    PlaceModule,
   ],
   providers: [
     {

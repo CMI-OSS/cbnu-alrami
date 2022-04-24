4f2ee2f2d4cb865331020d227629a95dfbc09225
vcho1958 <gusckd57@gmail.com>
2022-04-24 19:41:51 +0900

    chore: eslint warning solve

--- packages/server/src/board/board.controller.ts
+++ packages/server/src/board/board.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject, Param, ParseIntPipe } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
 
 import { BoardService } from "./board.service";
 

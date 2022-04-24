9879e55886d645c1efe2bd732bdae6f69ffa44c4
vcho1958 <gusckd57@gmail.com>
2022-04-24 19:40:01 +0900

    feat: admin entity beforeinsert로 비밀번호 해싱

--- packages/server/src/commons/entities/admin.entity.ts
+++ packages/server/src/commons/entities/admin.entity.ts
@@ -1,5 +1,6 @@
+import * as bcrypt from "bcrypt";
 import { Authority } from "src/commons/constants/enums";
-import { Column, Entity } from "typeorm";
+import { BeforeInsert, Column, Entity } from "typeorm";
 
 import { CommonEntity } from "./common.entity";
 
@@ -8,12 +9,23 @@ export class Admin extends CommonEntity {
   @Column("varchar", { length: 20, unique: true, nullable: false })
   loginId: string;
 
-  @Column("varchar", {nullable:false})
+  @Column("varchar", { nullable: false })
   password: string;
 
-  @Column("varchar", {length: 20, unique: true, nullable:false })
+  @Column("varchar", { length: 20, unique: true, nullable: false })
   nickname: string;
 
-  @Column({ type: "enum", enum: Authority, default: Authority.Guest, nullable: false })
+  @Column({
+    type: "enum",
+    enum: Authority,
+    default: Authority.Guest,
+    nullable: false,
+  })
   authority: Authority;
+
+  @BeforeInsert()
+  async setPassword(password: string) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(password || this.password, salt);
+  }
 }

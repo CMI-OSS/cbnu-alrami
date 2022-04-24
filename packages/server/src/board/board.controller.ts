7c95238d4a017941dba4ab47587ea4877d4def23
EunjiShin <eunji980310@gmail.com>
2022-04-24 22:44:46 +0900

    :sparkles: board API에 swagger description 추가

--- packages/server/src/board/board.controller.ts
+++ packages/server/src/board/board.controller.ts
@@ -9,6 +9,7 @@ import {
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common";
+import { ApiBody, ApiTags, ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { Public } from "src/commons/decorators/public.decorator";
 import { Board } from "src/commons/entities/board.entity";
 
@@ -16,33 +17,72 @@ import { BoardService } from "./board.service";
 import { BoardCreateDto } from "./dto/board.create.dto";
 import { BoardUpdateDto } from "./dto/board.update.dto";
 
+@Public()
 @Controller("boards")
+@ApiTags("board 도메인 API")
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
   @Post()
-  @Public()
   @UsePipes(ValidationPipe)
+  @ApiOperation({
+    summary: "board 생성 API",
+    description: "새로운 board를 생성한다.",
+  })
+  @ApiResponse({ status: 201, description: "생성된 board 객체", type: Board })
   async create(@Body() boardCreateDto: BoardCreateDto) {
     const board = await this.boardService.create(boardCreateDto);
     return board;
   }
 
   @Get()
-  @Public()
+  @ApiOperation({
+    summary: "board 전체 조회 API",
+    description: "모든 board를 조회한다.",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "전체 board 리스트",
+    type: Board,
+    isArray: true,
+  })
   async findAll(): Promise<Board[]> {
     return this.boardService.findAll();
   }
 
   @Get(":boardId")
-  @Public()
+  @ApiOperation({
+    summary: "특정 board 조회 API",
+    description: "id를 이용, 특정 board를 조회한다.",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "path variable를 pk로 가지는 board 객체",
+    type: Board,
+  })
   async findOne(@Param("boardId") boardId: number): Promise<Board> {
     return this.boardService.findById(boardId);
   }
 
   @Put(":boardId")
-  @Public()
   @UsePipes(ValidationPipe)
+  @ApiOperation({
+    summary: "특정 board 수정 API",
+    description: "특정 board를 수정한다.",
+  })
+  @ApiBody({
+    schema: {
+      properties: {
+        name: { type: "string" },
+        url: { type: "string" },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: "수정한 board 객체",
+    type: Board,
+  })
   async update(
     @Param("boardId") boardId: number,
     @Body() boardUpdateDto: BoardUpdateDto,
@@ -51,7 +91,15 @@ export class BoardController {
   }
 
   @Delete(":boardId")
-  @Public()
+  @ApiOperation({
+    summary: "특정 board 삭제 API",
+    description: "id를 이용, 특정 board를 삭제한다.",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "true",
+    type: "string",
+  })
   async remove(@Param("boardId") boardId: number) {
     return this.boardService.remove(boardId);
   }
--- packages/server/src/commons/entities/board.entity.ts
+++ packages/server/src/commons/entities/board.entity.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: "board" })
 export class Board {
   @PrimaryGeneratedColumn()
+  @ApiProperty({ description: "pk" })
   id!: number;
 
+  @ApiProperty({ description: "board 이름" })
   @Column({ type: "varchar", length: 100, unique: true })
   name!: string;
 
+  @ApiProperty({ description: "board url" })
   @Column({ type: "varchar", length: 255, unique: true })
   url!: string;
 }

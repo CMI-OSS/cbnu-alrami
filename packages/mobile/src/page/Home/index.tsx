abe8d34e374b121a7a8240dbf745b5d84aa428c6
gijin noh <38103082+nohgijin@users.noreply.github.com>
2022-04-30 17:26:41 +0900

    [mobile] 홈페이지 디자인 씽크 맞추기 (#192)
    * style: footer 수정
    
    * chore: 푸터에 패딩 추가
    
    * chore: 홈페이지 스타일 수정
    
    * style: 디자인 싱크 맞추기
    
    * refactor: 리뷰 반영
    
    * style: 홈 공지사항 스타일 sync
    
    * :bug: Fix lint error
    
    Co-authored-by: 제스 <jaryapp@gmail.com>

--- packages/mobile/src/page/Home/index.tsx
+++ packages/mobile/src/page/Home/index.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/jsx-pascal-case */
 import { Link } from "react-router-dom";
 
 import Footer from "@components/molecules/Footer";
-import Menu from "src/components/atoms/Chip";
 import { Arrow } from "src/components/atoms/icon/Arrow";
 
 import BorderBox from "../../components/atoms/BorderBox";
@@ -22,43 +20,43 @@ function Home() {
     "2022학년도 정시 법무대학원 신입생 추가",
     "2022학년도 정시 대학원(일반대학원) 석사과정",
     "산업인공지능연구센터 연구원 채용 공고",
-    "산업인공지능연구센터 연구원 채용 공고",
   ];
-  const menus = [ "최신공지", "인기공지" ];
 
   return (
     <section className={$.home}>
-      <header>
-        <div className={$.content}>
-          <h1>충림이</h1>
-          <span>오늘은 총 6개의 일정이 있어요</span>
+      <header className={$.header}>
+        <div className={$["header-content"]}>
+          <h1 className={$.title}>충림이</h1>
+          <p>오늘은 총 6개의 일정이 있어요</p>
         </div>
-        <Setting />
+        <button type="button">
+          <Setting />
+        </button>
       </header>
       <div className={$.schedule}>
         {schedules.map((schedule) => (
-          <BorderBox height={90}>
+          <BorderBox width={271} height={101}>
             <p>{schedule}</p>
             <Arrow />
           </BorderBox>
         ))}
       </div>
       <div className={$.information}>
-        <BorderBox height={160} background="#EAF4FE">
+        <BorderBox height={200} background="#EAF4FE">
           <비 style={{ width: "50px", height: "auto" }} />
-          <span>청주 날씨</span>
-          <span className={$.important}>25도</span>
-          <span>구름 조금</span>
+          <strong>청주 날씨</strong>
+          <span className={$.amount}>25도</span>
+          <span className={$.description}>구름 조금</span>
           <div className={$.more}>
             <Info style={{ width: "10px", height: "10px" }} />
             기온별 옷차림
           </div>
         </BorderBox>
-        <BorderBox height={160} background="#F2F0FE">
+        <BorderBox height={200} background="#F2F0FE">
           <Covid />
-          <span>확진자 수</span>
-          <span className={$.important}>500,000</span>
-          <span>+1,325</span>
+          <strong>확진자 수</strong>
+          <span className={$.amount}>500,000</span>
+          <span className={$.description}>+1,325</span>
           <Link className={$.more} to="/covid">
             더보기
             <Arrow style={{ width: "7px", height: "7px", stroke: "#222" }} />
@@ -66,15 +64,16 @@ function Home() {
         </BorderBox>
       </div>
       <div className={$.cafeteria}>
-        <BorderBox height={180}>
+        <BorderBox height={188}>
           <div className={$.title}>
-            <span>
-              본관 아침 <Write />
-            </span>
-            <span>7:30~9:00</span>
+            <div className={$.location}>
+              본관 아침
+              <Write />
+            </div>
+            <span className={$.time}>7:30~9:00</span>
           </div>
           <Line />
-          <div className={$.content}>
+          <div className={$["cafeteria-content"]}>
             흰밥/우유(두유)/김치 단호박스프 고구마치즈롤까스 &소스 양상추샐러드
             오리엔탈드레싱 시금치나물 에너지:1165Kcal 단백질:16g
           </div>
@@ -82,7 +81,15 @@ function Home() {
       </div>
       <div className={$.notification}>
         <BorderBox height={300}>
-          <div className={$.content}>
+          <div className={$.title}>
+            공지사항
+            <div className={$.category}>
+              <span>인기</span>
+              <span>최신</span>
+            </div>
+          </div>
+          <Line />
+          <div className={$["notification-content"]}>
             {notifications.map((notification) => (
               <p>{notification}</p>
             ))}

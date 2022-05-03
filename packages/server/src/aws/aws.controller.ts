ce92b4662acdaf5b40f187635a879188f12f8350
steve <ionc635@naver.com>
2022-05-03 22:44:02 +0900

    chore: 이미지 업로드 API swagger 적용

--- packages/server/src/aws/aws.controller.ts
+++ packages/server/src/aws/aws.controller.ts
@@ -5,17 +5,37 @@ import {
   UseInterceptors,
 } from "@nestjs/common";
 import { FilesInterceptor } from "@nestjs/platform-express";
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Public } from "src/commons/decorators/public.decorator";
 
 import { AwsService } from "./aws.service";
 
 @Controller()
+@ApiTags("[image] 이미지 업로드 API")
 export class AwsController {
   constructor(private readonly awsService: AwsService) {}
 
   @Public()
   @Post("upload/images")
   @UseInterceptors(FilesInterceptor("image", 10))
+  @ApiOperation({
+    summary: "이미지 업로드 API",
+    description: "이미지를 AWS S3에 업로드합니다.",
+  })
+  @ApiBody({
+    schema: {
+      example: {
+        image: [ "image.jpg", "image.png" ],
+      },
+      type: "Express.Multer.File[]",
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: "이미지 URI",
+    type: "string[]",
+    isArray: true,
+  })
   async uploadImages(
     @UploadedFiles() images: Express.Multer.File[],
   ): Promise<string[]> {

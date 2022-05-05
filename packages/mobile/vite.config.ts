4cd700d731c2bebf74cd5664e27b05d43e347986
gijin noh <38103082+nohgijin@users.noreply.github.com>
2022-05-05 18:53:57 +0900

    [front] webpack -> vite 번들링 도구 마이그레이션 (#212)
    * feat: vite 로 개발환경 변경
    - webpack을 제거하고 vite를 이용하여 개발환경 설정
    
    * feat: admin 페이지 번들러도구 변경
    - webpack에서 vite로 변경
    - logo, favicon 제거

---
+++ packages/mobile/vite.config.ts
@@ -0,0 +1,16 @@
+import * as path from 'path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve:{
+    alias:{
+      'src':path.resolve(__dirname,'./src'),
+      '@shared':path.resolve(__dirname,'../shared/src'),
+      '@components':path.resolve(__dirname,'./src/components')
+    }
+  },
+  plugins: [ react() ]
+})

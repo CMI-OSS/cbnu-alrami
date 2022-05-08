a5c8f636754202b884655b23cf0c959ecb455b96
EunjiShin <eunji980310@gmail.com>
2022-05-08 11:17:27 +0900

    :memo: boardTree swagger 추가

--- packages/server/src/boardTree/dto/boardTree.all.response.dto.ts
+++ packages/server/src/boardTree/dto/boardTree.all.response.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class BoardTreeAllResponseDto {
+  @ApiProperty({ description: "board id(pk값)" })
   id!: number;
+
+  @ApiProperty({ description: "board 이름" })
   name!: string;
+
+  @ApiProperty({ description: "board url" })
   url!: string;
+
+  @ApiProperty({ description: "하위 board" })
   children: BoardTreeAllResponseDto[];
 }

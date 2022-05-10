c62e2d5398360223e7e0659ede30152e2214a329
thdwlsgus0 <thdwlsgus0@naver.com>
2022-05-10 14:45:12 +0900

    style: map -> menu 추가(학교, 식사, 편리, 간식, 놀거리)

--- packages/mobile/src/page/Category/index.tsx
+++ packages/mobile/src/page/Category/index.tsx
@@ -9,6 +9,10 @@ import $ from "./style.module.scss";
 const menuList = [
   { id: 1, name: "학교", path: "/school" },
   { id: 2, name: "식사", path: "/food" },
+  { id: 3, name: "편리", path: "/convenient" },
+  { id: 2, name: "간식", path: "/food" },
+  { id: 2, name: "놀거리", path: "/food" },
+
 ];
 
 const hashList = [

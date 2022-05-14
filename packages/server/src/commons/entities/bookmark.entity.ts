90b9c51f94080cd23a32c8fd3ec04fa9c640a34f
Kim Jong Seo <80724255+ionc635@users.noreply.github.com>
2022-05-14 21:30:12 +0900

    [server] Bookmark Entity 생성 (#227)
    * feat: Bookmark Entity 생성
    
    * refact: 추가 보완

---
+++ packages/server/src/commons/entities/bookmark.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, ManyToOne } from "typeorm";
+
+import { Article } from "./article.entity";
+import { CommonEntity } from "./common.entity";
+import { User } from "./user.entity";
+
+@Entity({ name: "bookmark" })
+export class Bookmark extends CommonEntity {
+  @ManyToOne(() => User, (User) => User.id, { cascade: true, nullable: false })
+  user: number;
+
+  @ManyToOne(() => Article, (Article) => Article.id, {
+    cascade: true,
+    nullable: false,
+  })
+  article: number;
+}

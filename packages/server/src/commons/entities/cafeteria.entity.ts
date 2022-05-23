b2fd63e55ea6538b4e433da83f423ec3386952c0
vcho1958 <gusckd57@gmail.com>
2022-05-23 10:02:52 +0900

    feat: add Cafeteria entity

--- packages/server/src/commons/entities/cafeteria.entity.ts
+++ packages/server/src/commons/entities/cafeteria.entity.ts
@@ -1,21 +1,30 @@
-import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from "typeorm";
 
+import { CafeteriaMenu } from "./cafeteriaMenu.entity";
 import { CommonEntity } from "./common.entity";
 import { Place } from "./place.entity";
 
 @Entity()
 export class Cafeteria extends CommonEntity {
-  @OneToOne(() => Place)
+  @OneToOne(() => Place, {
+    eager: true,
+    onDelete: "CASCADE",
+    nullable: false,
+    onUpdate: "CASCADE",
+    orphanedRowAction: "delete",
+  })
   @JoinColumn()
   place: Place;
 
-  @Column('varchar', { nullable: false })
-  breakfastTimeInfo: string;
+  @Column("varchar", { nullable: true, default: null })
+  breakfastTimeInfo?: string;
 
-  @Column('varchar', { nullable: false })
-  lunchTimeInfo: string;
+  @Column("varchar", { nullable: true, default: null })
+  lunchTimeInfo?: string;
 
-  @Column('varchar', { nullable: false })
-  dinnerTimeInfo: string;
+  @Column("varchar", { nullable: true, default: null })
+  dinnerTimeInfo?: string;
 
+  @OneToMany(() => CafeteriaMenu, (cafeteriaMenu) => cafeteriaMenu.cafeteria)
+  cafeteriaMenus: CafeteriaMenu[];
 }

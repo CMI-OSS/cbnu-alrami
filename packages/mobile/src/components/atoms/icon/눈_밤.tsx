38b88511d47450122db702e9256b1bbeda67a6ac
nohgijin <ynda11111@naver.com>
2022-05-24 19:35:21 +0900

    feat: 시간/날씨별 아이콘 조회

---
+++ packages/mobile/src/components/atoms/icon/눈_밤.tsx
@@ -0,0 +1,79 @@
+import type { StyleProps } from "src/type/props";
+
+function 눈_밤({ className, style }: StyleProps) {
+  return (
+    <svg
+      {...{ className, style }}
+      width="54"
+      height="59"
+      viewBox="0 0 54 59"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M26.7622 3V56.5244"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.116 6.82349L26.0552 13.7627C26.4457 14.1532 27.0789 14.1532 27.4694 13.7627L34.4087 6.82349"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M34.4087 52.7009L27.4695 45.7617C27.0789 45.3712 26.4458 45.3712 26.0552 45.7617L19.116 52.7009"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.82324 17.2683L50.1767 44.0305"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.31104 25.8015L12.7902 23.2616C13.3237 23.1186 13.6403 22.5703 13.4973 22.0368L10.9574 12.5577"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M50.6887 35.4969L41.2095 38.0369C40.6761 38.1798 40.3595 38.7282 40.5024 39.2616L43.0424 48.7408"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.82373 44.031L50.1772 17.2688"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.958 48.741L13.4979 39.2618C13.6409 38.7283 13.3243 38.18 12.7908 38.037L3.31166 35.4971"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M43.043 12.5579L40.503 22.037C40.3601 22.5705 40.6767 23.1188 41.2101 23.2618L50.6893 25.8017"
+        stroke="#B6DFF7"
+        strokeWidth="4.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+export { 눈_밤 };
--- packages/mobile/src/page/Home/Weather/index.tsx
+++ packages/mobile/src/page/Home/Weather/index.tsx
@@ -1,58 +1,119 @@
 /* eslint-disable react/jsx-pascal-case */
 
+import dayjs from "dayjs";
+import find from "lodash/find";
 import BorderBox from "src/components/atoms/BorderBox";
-import { Info, 구름조금_낮 } from "src/components/atoms/icon";
+import {
+  Info,
+  가끔눈_낮,
+  가끔눈_밤,
+  가끔눈또는비_낮,
+  가끔눈또는비_밤,
+  가끔비_낮,
+  가끔비_밤,
+  가끔비또는눈_낮,
+  가끔비또는눈_밤,
+  구름많음_낮,
+  구름많음_밤,
+  구름조금_낮,
+  구름조금_밤,
+  눈_낮,
+  눈_밤,
+  눈또는비,
+  맑음_낮,
+  맑음_밤,
+  박무,
+  비,
+  비또는눈,
+  소나기,
+  안개,
+  연무,
+  천둥번개,
+  황사,
+  흐림,
+} from "src/components/atoms/icon";
 
 import $ from "./style.module.scss";
 
-const iconToBackground = new Map();
-iconToBackground.set("#E0F0FF", [
-  "맑음_낮",
-  "구름조금_낮",
-  "구름많음_낮",
-  "눈",
-  "가끔눈_낮",
-]);
-iconToBackground.set("#E9ECF2", [
-  "흐림",
-  "비",
-  "비또는눈",
-  "눈또는비",
-  "천둥번개",
-  "안개",
-  "소나기",
-  "가끔비_낮",
-  "가끔비또는눈_낮",
-  "가끔비_밤",
-  "가끔비또는눈_밤",
-  "가끔눈또는비_낮",
-  "가끔눈또는비_밤",
-  "연무",
-  "박무",
-]);
-iconToBackground.set("#E6ECFA", [
-  "맑음_밤",
-  "구름조금_밤",
-  "구름많음_밤",
-  "눈",
-  "가끔눈_밤",
-]);
-iconToBackground.set("#ECEAE6", [ "천둥번개" ]);
-
-const targetIcon = "맑음_밤";
+const colors = {
+  morning: "#E0F0FF",
+  night: "#E6ECFA",
+  cloudy: "#E9ECF2",
+  dusty: "#ECEAE6",
+};
+const iconToBackgrounds = [
+  { backgroundColor: colors.morning, icon: <가끔눈_낮 />, key: "가끔눈_낮" },
+  { backgroundColor: colors.night, icon: <가끔눈_밤 />, key: "가끔눈_밤" },
+  {
+    backgroundColor: colors.cloudy,
+    icon: <가끔눈또는비_낮 />,
+    key: "가끔눈또는비_낮",
+  },
+  {
+    backgroundColor: colors.cloudy,
+    icon: <가끔눈또는비_밤 />,
+    key: "가끔눈또는비_밤",
+  },
+  { backgroundColor: colors.cloudy, icon: <가끔비_낮 />, key: "가끔비_낮" },
+  { backgroundColor: colors.cloudy, icon: <가끔비_밤 />, key: "가끔비_밤" },
+  {
+    backgroundColor: colors.cloudy,
+    icon: <가끔비또는눈_낮 />,
+    key: "가끔비또는눈_낮",
+  },
+  {
+    backgroundColor: colors.cloudy,
+    icon: <가끔비또는눈_밤 />,
+    key: "가끔비또는눈_밤",
+  },
+  {
+    backgroundColor: colors.morning,
+    icon: <구름많음_낮 />,
+    key: "구름많음_낮",
+  },
+  { backgroundColor: colors.night, icon: <구름많음_밤 />, key: "구름많음_밤" },
+  {
+    backgroundColor: colors.morning,
+    icon: <구름조금_낮 />,
+    key: "구름조금_낮",
+  },
+  { backgroundColor: colors.night, icon: <구름조금_밤 />, key: "구름조금_밤" },
+  { backgroundColor: colors.morning, icon: <눈_낮 />, key: "눈_낮" },
+  { backgroundColor: colors.night, icon: <눈_밤 />, key: "눈_밤" },
+  { backgroundColor: colors.cloudy, icon: <눈또는비 />, key: "눈또는비" },
+  { backgroundColor: colors.morning, icon: <맑음_낮 />, key: "맑음_낮" },
+  { backgroundColor: colors.night, icon: <맑음_밤 />, key: "맑음_밤" },
+  { backgroundColor: colors.cloudy, icon: <박무 />, key: "박무" },
+  { backgroundColor: colors.cloudy, icon: <비 />, key: "비" },
+  { backgroundColor: colors.cloudy, icon: <비또는눈 />, key: "비또는눈" },
+  { backgroundColor: colors.cloudy, icon: <소나기 />, key: "소나기" },
+  { backgroundColor: colors.cloudy, icon: <안개 />, key: "안개" },
+  { backgroundColor: colors.cloudy, icon: <연무 />, key: "연무" },
+  { backgroundColor: colors.cloudy, icon: <천둥번개 />, key: "천둥번개" },
+  { backgroundColor: colors.dusty, icon: <황사 />, key: "황사" },
+  { backgroundColor: colors.cloudy, icon: <흐림 />, key: "흐림" },
+];
 
 function Weather() {
+  const time = dayjs().hour() >= 12 ? "밤" : "낮";
+  const timeTarget = find(
+    iconToBackgrounds,
+    (o) => o.key === `천둥번개${time}`,
+  );
+  const target = find(iconToBackgrounds, (o) => o.key === `천둥번개`);
+  const iconToBackground = timeTarget ?? target;
+
   return (
     <div className={$.weather}>
-      <BorderBox height={155} background="#EAF4FE">
+      <BorderBox height={155} background={iconToBackground?.backgroundColor}>
         <div className={$.first}>
-          <span className={$.amount}>25°C</span>림
+          <span className={$.amount}>25°C</span>흐림
           <span className={$.description}>청주, 구름 조금</span>
           <span className={$.celsius}>-11.0 °C / 2.0°C</span>
         </div>
         <div className={$.second}>
           <div style={{ width: "66px", height: "55px" }}>
-            <구름조금_낮 />
+            {iconToBackground?.icon}
           </div>
           <div className={$.more}>
             <Info width="14" height="14" />

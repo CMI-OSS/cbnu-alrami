9de1cb2b3a170d9991473538a4ffc6f54d7794ef
Kim Jong Seo <80724255+ionc635@users.noreply.github.com>
2022-05-29 13:32:17 +0900

    [server] Pipe 전역 설정 (#245)
    * feat: Pipe 전역 설정
    
    * feat: Request DTO 내 class-validator 추가 적용
    
    * fix: Jenkins error 해결
    
    * fix: 코드 리뷰 반영

--- packages/server/src/admin/dto/adminCreate.dto.ts
+++ packages/server/src/admin/dto/adminCreate.dto.ts
@@ -1,15 +1,23 @@
-import { Matches } from "class-validator";
+import { IsEnum, IsNotEmpty, IsString, Matches } from "class-validator";
 import { Authority } from "src/commons/constants/enums";
 
 export class AdminCreateDto {
+  @IsNotEmpty()
+  @IsString()
   @Matches(/^[a-z0-9_]{4,18}$/)
   loginId: string;
 
+  @IsNotEmpty()
+  @IsString()
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/)
   password: string;
 
+  @IsNotEmpty()
+  @IsString()
   @Matches(/^[a-z0-9_]{2,18}$/)
   nickname: string;
 
+  @IsNotEmpty()
+  @IsEnum(Authority)
   authority: Authority;
 }
--- packages/server/src/weather/dtos/create-weather.request.dto.ts
+++ packages/server/src/weather/dtos/create-weather.request.dto.ts
@@ -1,28 +1,34 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateWeatherRequestDto {
   @IsNotEmpty()
+  @IsString()
   @ApiProperty({ description: "현재 날씨" })
   currentWeather: string;
 
   @IsNotEmpty()
+  @IsNumber()
   @ApiProperty({ description: "현재 온도" })
   currentTemp: number;
 
   @IsNotEmpty()
+  @IsNumber()
   @ApiProperty({ description: "오늘 최고 온도" })
   maxTemp: number;
 
   @IsNotEmpty()
+  @IsNumber()
   @ApiProperty({ description: "오늘 최저 온도" })
   minTemp: number;
 
   @IsNotEmpty()
+  @IsString()
   @ApiProperty({ description: "오전 날씨" })
   amWeather: string;
 
   @IsNotEmpty()
+  @IsString()
   @ApiProperty({ description: "오후 날씨" })
   pmWeather: string;
 
@@ -31,6 +37,7 @@ export class CreateWeatherRequestDto {
   date: Date;
 
   @IsNotEmpty()
+  @IsNumber()
   @ApiProperty({ description: "시간 단위" })
   hour: number;
 }

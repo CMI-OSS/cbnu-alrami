20695784780abd63eb65cbed083385b324cdcc03
thdwlsgus0 <thdwlsgus0@naver.com>
2022-06-08 11:34:49 +0900

    style: 제보하기 header 작업

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -7,6 +7,7 @@ import queryString from "query-string";
 
 import Cafeteria from "./page/Cafeteria";
 import Calendar from "./page/Calendar";
+import Call from "./page/Call";
 import Detail from "./page/Detail";
 import Home from "./page/Home";
 import Map from "./page/Map";
@@ -23,6 +24,7 @@ function App() {
     { id: 4, path: "/cafeteria", element: <Cafeteria /> },
     { id: 5, path: "/map", element: <Map /> },
     { id: 6, path: "/more", element: <MoreImage /> },
+    { id: 7, path: "/call", element: <Call /> },
   ];
 
   return (
---
+++ packages/mobile/src/page/Call/index.tsx
@@ -0,0 +1,9 @@
+import MapHeader from "@components/molecules/MapHeader";
+
+import $ from "./style.module.scss";
+
+function Call() {
+  return <MapHeader title="제보하기" />;
+}
+
+export default Call;

3274ab86969d21a705b24aff3382b02c6412ba3c
thdwlsgus0 <thdwlsgus0@naver.com>
2022-06-10 11:32:28 +0900

    feat: onChage 상태 추가

--- packages/mobile/src/page/Call/index.tsx
+++ packages/mobile/src/page/Call/index.tsx
@@ -1,14 +1,22 @@
+import React, { useState } from "react";
+
 import MapHeader from "@components/molecules/MapHeader";
 
 import $ from "./style.module.scss";
 
 function Call() {
+  const [ textarea, setTextarea ] = useState("");
+  const handleTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextarea(e.target.value);
+  };
   return (
     <>
       <MapHeader title="제보하기" />
       <textarea
         placeholder="내용을 입력해주세요"
         className={$.textarea}
+        onChange={handleTextarea}
+        value={textarea}
       ></textarea>
     </>
   );

631eb6df6434396a9f3c027c22f81e50903f2fdc
nohgijin <ynda11111@naver.com>
2022-06-12 20:51:58 +0900

    markup: 가이드페이지, 공지사항 리스트 페이지 마크업

--- packages/mobile/src/page/Notification/index.tsx
+++ packages/mobile/src/page/Notification/index.tsx
@@ -1,10 +1,19 @@
 import { NavLink } from "react-router-dom";
 
+import CardList from "@components/molecules/CardList";
 import Footer from "@components/molecules/Footer";
 import classNames from "classnames";
 import { Setting, Plus, Star } from "src/components/atoms/icon";
+import useSearch from "src/hooks/useSearch";
 
 import $ from "./style.module.scss";
+
+function EmptyGuideImage({ major }: { major: string }) {
+  if (major === "즐겨찾기") {
+    return <div>즐겨찾기없는가이드이미지</div>;
+  }
+  return <div>공지없는가이드이미지</div>;
+}
 
 function Notification() {
   const mockCategory = [
@@ -14,6 +23,37 @@ function Notification() {
     { major: "어떤학과", to: "?major=어떤학과" },
     { major: "이런학과저런학과", to: "?major=이런학과저런학과" },
   ];
+
+  const mockNotification = [
+    {
+      id: 1,
+      category: "충북대학교",
+      title: "충북대학교 인문대학 영상 공모전 안내1",
+      date: "22-04-22",
+      view: 123,
+      star: 55,
+    },
+    {
+      id: 2,
+      category: "충북대학교",
+      title: "충북대학교 인문대학 영상 공모전 안내2",
+      date: "22-04-22",
+      view: 123,
+      star: 55,
+    },
+    {
+      id: 3,
+      category: "충북대학교",
+      title: "충북대학교 인문대학 영상 공모전 안내3",
+      date: "22-04-22",
+      view: 123,
+      star: 55,
+    },
+  ];
+
+  const mockSubscribeNotification = 1;
+  const mockStarNotification = 2;
+  const major = useSearch({ target: "major" })!;
 
   return (
     <section className={$.notification}>
@@ -43,6 +83,14 @@ function Notification() {
             {category.major}
           </NavLink>
         ))}
+      </div>
+      <div className={$["notification-list"]}>
+        {(major === "즐겨찾기" && !mockStarNotification) ||
+        (major !== "즐겨찾기" && !mockSubscribeNotification) ? (
+          <EmptyGuideImage {...{ major }} />
+        ) : (
+          <CardList notifications={mockNotification} />
+        )}
       </div>
       <Footer />
     </section>

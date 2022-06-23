f987de07b30ddbcdac728d6b45e36903e39205f2
thdwlsgus0 <thdwlsgus0@naver.com>
2022-06-23 15:48:40 +0900

    feat: 현재위치 가져오는 로직 구현 및 적용

--- packages/mobile/src/page/Map/index.tsx
+++ packages/mobile/src/page/Map/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import { Close } from "@components/atoms/icon/Close";
@@ -24,19 +24,64 @@ const makeMarker = (map: naver.maps.Map, position: naver.maps.LatLng) => {
 };
 
 function Map() {
-  const CBNU_LATITUDE = 36.62903849870408;
-  const CBNU_LONGITUDE = 127.45635082700974;
+  const CBNU_LATITUDE = 36.62850496903595;
+  const CBNU_LONGITUDE = 127.45731862757414;
 
+  const [ myLocation, setMyLocation ] = useState<
+    { latitude: number; longitude: number } | string
+  >("");
   const { isDisplayFloatingButton, isDisplayTooltip } = useAppSelector(
     (state) => state.statusReducer.map,
   );
   const dispatch = useAppDispatch();
 
+  const comparePosition = (latitude: number, longitude: number) => {
+    return (
+      latitude >= 36.62 &&
+      latitude <= 36.63 &&
+      longitude >= 127.44 &&
+      longitude <= 127.46
+    );
+  };
+
   useEffect(() => {
     const initMap = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            setMyLocation({
+              latitude: comparePosition(
+                position.coords.latitude,
+                position.coords.longitude,
+              )
+                ? position.coords.latitude
+                : CBNU_LATITUDE,
+              longitude: comparePosition(
+                position.coords.latitude,
+                position.coords.longitude,
+              )
+                ? position.coords.longitude
+                : CBNU_LONGITUDE,
+            });
+          },
+        );
+      } else {
+        alert("현재 위치를 알 수 없습니다.");
+      }
+    };
+    initMap();
+  }, []);
+
+  useEffect(() => {
+    if (typeof myLocation !== "string") {
+      const currentPosition = [ myLocation.latitude, myLocation.longitude ];
       const map = new naver.maps.Map("map", {
-        center: new naver.maps.LatLng(CBNU_LATITUDE, CBNU_LONGITUDE),
-        zoom: 18,
+        center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
+        zoom: 16,
+        zoomControl: true,
+        zoomControlOptions: {
+          position: naver.maps.Position.TOP_RIGHT,
+        },
       });
       placeInfoList.forEach((place) => {
         const marker = makeMarker(
@@ -51,10 +96,8 @@ function Map() {
           );
         });
       });
-      return map;
-    };
-    initMap();
-  }, []);
+    }
+  }, [ myLocation ]);
 
   return (
     <div id="map" className={$.map}>

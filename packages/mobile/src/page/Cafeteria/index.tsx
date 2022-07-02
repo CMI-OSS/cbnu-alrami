be4f1e4abbcc8815ae269cc96d95fb9556307c11
동동이 <drdragon777@naver.com>
2022-07-03 03:02:26 +0900

    feat: add Cafeteria page

--- packages/mobile/src/page/Cafeteria/index.tsx
+++ packages/mobile/src/page/Cafeteria/index.tsx
@@ -1,7 +1,50 @@
+import { Link } from "react-router-dom";
+
+import { cafeteriaMenu } from "src/__mocks__";
+import { Arrow } from "src/components/atoms/icon/Arrow";
+import CafeteriaMenuCard from "src/components/molecules/CafeteriaMenuCard";
+import Flicking from "src/components/molecules/Flicking";
+import Footer from "src/components/molecules/Footer";
+import { cafeteriaTime } from "src/utils/cafeteriaTime";
+
 import $ from "./style.module.scss";
 
+const menuList = [
+  { id: 1, name: "본관" },
+  { id: 2, name: "양성재" },
+  { id: 3, name: "양진재" },
+  { id: 4, name: "한빛식당" },
+  { id: 5, name: "별빛식당" },
+  { id: 6, name: "은하수식당" },
+];
+
 function Cafeteria() {
-  return <section className={$.cafeteria}>Cafeteria</section>;
+  return (
+    <>
+      <Flicking menuList={menuList} />
+      <main className={$.cafeteria}>
+        {cafeteriaMenu.map(({ id, content, calory, time }) => {
+          const [ mealTime, timeInfo ] = cafeteriaTime(id, time);
+          return (
+            <CafeteriaMenuCard
+              key={content}
+              {...{ mealTime, timeInfo }}
+              mealMenu={content}
+              calory={calory}
+            />
+          );
+        })}
+        <div className={$["go-out"]}>
+          <span>다른 메뉴가 먹고싶다면?</span>
+          <Link to="/place/food" className={$["go-out-link"]}>
+            나가서 먹기
+            <Arrow className={$.icon} />
+          </Link>
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
 }
 
 export default Cafeteria;

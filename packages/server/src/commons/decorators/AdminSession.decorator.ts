5ad14d722962adeda38de91b10c372be12c332ac
제스 <49256790+jaryapp@users.noreply.github.com>
2022-07-16 03:44:51 +0900

    [server] 세션 데코레이터 (#300)

--- packages/server/src/commons/decorators/AdminSession.decorator.ts
+++ packages/server/src/commons/decorators/AdminSession.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { AdminCredential } from "src/auth/dto/adminCredential.dto";
 
-export const UserField = createParamDecorator(
+export const AdminSession = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): AdminCredential => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;

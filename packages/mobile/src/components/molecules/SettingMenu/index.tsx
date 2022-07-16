e52dbddfcb3b40c08cabaefc40b091281b7300b4
동동이 <drdragon777@naver.com>
2022-07-17 03:32:23 +0900

    feat: add SettingMenu props

--- packages/mobile/src/components/molecules/SettingMenu/index.tsx
+++ packages/mobile/src/components/molecules/SettingMenu/index.tsx
@@ -8,17 +8,19 @@ interface Props {
     icon: any;
     label: string;
     to: string;
+    width: string;
+    height: string;
   };
   config?: string;
 }
 
 export default function SettingMenu({ route, config }: Props) {
-  const { id, label, to } = route;
+  const { id, label, to, width, height } = route;
 
   return (
     <NavLink key={id} to={to} className={$["setting-menu"]}>
       <div className={$["icon-box"]}>
-        <route.icon className={$.icon} />
+        <route.icon className={$.icon} style={{ width, height }} />
       </div>
       <span>{label}</span>
       <span className={$.config}>{config}</span>

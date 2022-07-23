93f4f173dee72e4bd39cd87477fcbcfc4f627092
동동이 <drdragon777@naver.com>
2022-07-24 01:15:32 +0900

    feat: add RadioSelect group

--- packages/mobile/src/page/SettingCafeteria/index.tsx
+++ packages/mobile/src/page/SettingCafeteria/index.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
+import BorderBox from "src/components/atoms/BorderBox";
 import { Food } from "src/components/atoms/icon";
+import RadioSelect from "src/components/molecules/RadioSelect";
 import SettingTemplate from "src/components/templates/SettingTemplate";
 
 import $ from "./style.module.scss";
 
+const cafeterias = [
+  "표시 안함",
+  "본관",
+  "양성재",
+  "양진재",
+  "별빛식당",
+  "은하수식당",
+  "한빛식당",
+];
+
 export default function SettingCafeteria() {
+  const [ representative, setRepresentative ] = useState("표시 안함");
+
   return (
     <SettingTemplate title="대표 식당">
       <div className={$["setting-cafeteria"]}>
@@ -14,6 +30,18 @@ export default function SettingCafeteria() {
           식당을 선택해주세요
         </span>
       </div>
+
+      <BorderBox className={$["select-cafeteria"]}>
+        {cafeterias.map((label) => (
+          <RadioSelect
+            key={label}
+            label={label}
+            className={$["radio-select"]}
+            isChecked={representative === label}
+            handleChange={setRepresentative}
+          />
+        ))}
+      </BorderBox>
     </SettingTemplate>
   );
 }

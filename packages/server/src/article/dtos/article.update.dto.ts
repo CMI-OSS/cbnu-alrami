3f618520e057059c05ae2237a366daa868eb4f56
우디 <38103085+EunjiShin@users.noreply.github.com>
2022-07-31 21:14:06 +0900

    [server] article 수정 API에 image 수정 로직 추가 (#363)
    * :sparkles: article update 모듈이 세션 데코레이터 사용하도록 수정
    
    * :sparkles: article 생성 API가 세션 데코레이터 사용하도록 수정
    
    * :sparkles: url 빈 값 허용
    
    * :sparkles: 공지사항 이미지 수정 로직 추가
    
    * :sparkles: article image update 로직 수정
    
    * :sparkles: article update할 때 맵핑된 article image 전체 삭제하는 경우 추가
    
    * :recycle: 불필요한 조건 체크 삭제
    
    * :recycle: article_image 삭제 시 imageId 이용하도록 수정

--- packages/server/src/article/dtos/article.update.dto.ts
+++ packages/server/src/article/dtos/article.update.dto.ts
@@ -1,16 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, IsNumber } from "class-validator";
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from "class-validator";
 
 export class ArticleUpdateDto {
   @IsNotEmpty()
   @IsNumber()
   @ApiProperty({ description: "소속 공지사항 사이트 id (pk)" })
   boardId!: number;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @ApiProperty({ description: "작성자 id (pk)" })
-  adminId!: number;
 
   @IsNotEmpty()
   @IsString()
@@ -22,12 +23,16 @@ export class ArticleUpdateDto {
   @ApiProperty({ description: "공지사항 내용" })
   content!: string;
 
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
   @ApiProperty({ description: "공지사항 url" })
-  url!: string;
+  url: string;
 
   @IsNotEmpty()
   @ApiProperty({ description: "공지사항 등록 날짜 : YYYY-MM-DD" })
   date!: Date;
+
+  @IsArray()
+  @ApiProperty({ description: "공지사항에 첨부된 이미지 id 배열" })
+  images: number[];
 }

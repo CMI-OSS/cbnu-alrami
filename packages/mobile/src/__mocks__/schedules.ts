b3ab8cc18787f41bee5ad9a7a0a636120d010a5d
HyunoChoi <soonitoon@gmail.com>
2022-08-01 13:17:01 +0900

    [mobile] 일정 페이지에서 개인일정을 즐겨찾기로 대체하기 (#374)
    * refactor: Change personal schedule to stared schedule
    
    * refactor: Replace SVG + span to single png file
    
    * chore: Change stared to favorites
    
    * chore: Change favorites to bookmark

--- packages/mobile/src/__mocks__/schedules.ts
+++ packages/mobile/src/__mocks__/schedules.ts
@@ -65,31 +65,31 @@ const COLLEGE_SCHEDULES = [
   },
 ];
 
-const PERSONAL_SCHEDULES = [
+const STARED_SCHEDULES = [
   {
     id: 0,
-    content: "개인 일정",
-    priority: 1,
+    content: "학사일정",
+    priority: 0,
     isHoliyday: 0 as const,
-    startDate: "2022-06-20T10:30:00",
-    endDate: "2022-06-22T09:00:00",
+    startDate: "2022-05-30T00:00:00",
+    endDate: "2022-06-10T00:00:00",
   },
   {
     id: 1,
-    content: "공휴일",
+    content: "학사일정",
     priority: 0,
-    isHoliyday: 1 as const,
-    startDate: "2022-06-15T00:00:00",
-    endDate: null,
+    isHoliyday: 0 as const,
+    startDate: "2022-05-30T00:00:00",
+    endDate: "2022-06-10T00:00:00",
   },
   {
     id: 2,
-    content: "하루 일정",
+    content: "학사일정",
     priority: 0,
     isHoliyday: 0 as const,
-    startDate: "2022-06-24T09:00:00",
-    endDate: "2022-06-24T10:00:00",
+    startDate: "2022-05-30T00:00:00",
+    endDate: "2022-06-10T00:00:00",
   },
 ];
 
-export { COLLEGE_SCHEDULES, PERSONAL_SCHEDULES };
+export { COLLEGE_SCHEDULES, STARED_SCHEDULES as PERSONAL_SCHEDULES };
--- packages/mobile/src/page/Calendar/CardBox/index.tsx
+++ packages/mobile/src/page/Calendar/CardBox/index.tsx
@@ -1,12 +1,11 @@
 import { animateScroll } from "react-scroll";
 
-import { Dayjs } from "dayjs";
+import guideEmptyFavoritesSchedule from "src/assets/guide_empty_favorites_schedule.png";
 import { Arrow } from "src/components/atoms/icon/Arrow";
 import useScroll from "src/hooks/useScroll";
 import { Schedule, ScheduleType } from "src/page/Calendar";
 
 import CollegeCard from "../CollegeCard";
-import PersonalCard from "../PersonalCard";
 import $ from "./style.module.scss";
 
 const CALLENDAR_UNVISIBLE_POINT = 320;
@@ -14,36 +13,31 @@ const CALLENDAR_UNVISIBLE_POINT = 320;
 type Props = {
   scheduleType: ScheduleType;
   schedules: Schedule[];
-  selectedDate: Dayjs;
 };
 
-function CardBox({ scheduleType, schedules, selectedDate }: Props) {
+function CardBox({ scheduleType, schedules }: Props) {
   const { Y } = useScroll();
 
   if (schedules.length === 0)
     return (
       <section className={$["empty-box"]}>
-        <span className={$.discription}>오늘은 일정이 없어요</span>
+        {scheduleType === "all" ? (
+          <span className={$.discription}>오늘은 일정이 없어요</span>
+        ) : (
+          <img
+            className={$["empty-bookmark-board"]}
+            src={guideEmptyFavoritesSchedule}
+            alt="즐겨찾기된 학사일정 없음"
+          />
+        )}
       </section>
     );
 
   return (
     <section className={$["card-box"]}>
-      {schedules.map(({ id, content, startDate, endDate }) =>
-        scheduleType === "college" ? (
-          <CollegeCard key={id} {...{ content, startDate, endDate }} />
-        ) : (
-          <PersonalCard
-            key={id}
-            {...{
-              content,
-              startDate,
-              endDate,
-              selectedDate,
-            }}
-          />
-        ),
-      )}
+      {schedules.map(({ id, content, startDate, endDate }) => (
+        <CollegeCard key={id} {...{ content, startDate, endDate }} />
+      ))}
       {Y > CALLENDAR_UNVISIBLE_POINT && (
         <button
           className={$["floating-button"]}

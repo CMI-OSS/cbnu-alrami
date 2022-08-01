5c9473dec46447b68690baf8da32614203491a90
제스 <49256790+jaryapp@users.noreply.github.com>
2022-08-01 23:45:57 +0900

    ✨ Add onDelete CASCADE for FK (#379)

--- packages/server/src/commons/entities/boardTree.entity.ts
+++ packages/server/src/commons/entities/boardTree.entity.ts
@@ -1,16 +1,15 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, JoinColumn, OneToOne } from "typeorm";
 
 import { Board } from "./board.entity";
 import { CommonEntity } from "./common.entity";
 
 @Entity("board_tree")
 export class BoardTree extends CommonEntity {
-  @OneToOne(() => Board)
+  @OneToOne(() => Board, { onDelete: "CASCADE" })
   @JoinColumn()
   board: Board;
 
-  @OneToOne(() => Board)
+  @OneToOne(() => Board, { onDelete: "CASCADE" })
   @JoinColumn()
   parentBoard: Board;
 }

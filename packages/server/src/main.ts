6668b2b6513f3f60f3408174b3a128ada48bbbf7
제스 <49256790+jaryapp@users.noreply.github.com>
2022-08-03 08:44:29 +0900

    [server] 요청 크기 제한 늘리기 #385

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -18,8 +18,8 @@ async function bootstrap() {
   app.enableCors({
     origin: "*",
   });
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ limit: "50mb", extended: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({

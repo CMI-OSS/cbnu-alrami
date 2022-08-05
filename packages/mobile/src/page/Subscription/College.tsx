aee40e14a31f44a67ad93dd9c14d267bdc0c8394
nohgijin <ynda11111@naver.com>
2022-08-05 11:36:05 +0900

    fix: 에러 해결

--- packages/mobile/src/page/Subscription/College.tsx
+++ packages/mobile/src/page/Subscription/College.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import BorderBox from "@components/atoms/BorderBox";
 import { useCollegeBoardTree } from "src/api/boardTree";

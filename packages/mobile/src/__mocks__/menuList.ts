6878d5d3be7b2f48fbcc277e1b5d9f225e0bad1b
Jin <thdwlsgus0@naver.com>
2022-08-07 00:28:59 +0900

    [mobile] atomic 디자인에 맞게 상태 로직 제거 필요 (#388)
    * refactor: chipGroup 리팩토링
    
    * refactor: placeDetail 구현
    
    * refactor: route 위치 수정
    
    * refactor: 사용하지 않는 파일 삭제
    
    * feat: redux-persist 적용
    
    * fix: persist 적용 안되는 부분 수정
    
    * refactor: 코드리뷰 반영
    
    * fix: 빌드 수정

--- packages/mobile/src/__mocks__/menuList.ts
+++ packages/mobile/src/__mocks__/menuList.ts
@@ -1,8 +1,8 @@
 const menuList = [
-  { id: 1, name: "전체", path: "/place/all" },
-  { id: 2, name: "N구역", path: "/place/north" },
-  { id: 3, name: "E구역", path: "/place/east" },
-  { id: 4, name: "S구역", path: "/place/south" },
+  { id: 1, name: "전체", path: "?position=all" },
+  { id: 2, name: "N구역", path: "?position=north" },
+  { id: 3, name: "E구역", path: "?position=east" },
+  { id: 4, name: "S구역", path: "?position=south" },
 ];
 
 export default menuList;

6003ee3e085969afa752f7c31ab87caec4355327
EunjiShin <eunji980310@gmail.com>
2022-08-07 16:48:07 +0900

    :sparkles: page response에 마지막 페이지 플래그 추가

--- packages/server/src/commons/page/page.ts
+++ packages/server/src/commons/page/page.ts
@@ -1,15 +1,11 @@
 import { PageRequest } from "./page.request";
 
 export class Page {
-  // 현재 페이지 인덱스
-  currentPageNo: number;
-  // 현제 한 페이지 사이즈
-  currentPageSize: number;
-  // 현제 페이지 사이즈에서 전체 페이지 수
-  totalPageCount: number;
+  // 마지막 페이지 확인 플래그
+  isEnd: boolean;
   constructor(pageRequest: PageRequest, totalItemCount: number) {
-    this.currentPageNo = pageRequest.pageNo;
-    this.currentPageSize = pageRequest.pageSize;
-    this.totalPageCount = Math.ceil(totalItemCount / pageRequest.pageSize);
+    const currentPageNo = pageRequest.pageNo;
+    const totalPageCount = Math.ceil(totalItemCount / pageRequest.pageSize);
+    this.isEnd = currentPageNo === totalPageCount;
   }
 }

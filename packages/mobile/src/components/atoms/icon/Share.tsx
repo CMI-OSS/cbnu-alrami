bb852fc787deddd8fe0dd57656de00174c2c402a
nohgijin <ynda11111@naver.com>
2022-08-08 20:59:56 +0900

    refactor: 추가 아이콘 변경

--- packages/mobile/src/components/atoms/icon/Share.tsx
+++ packages/mobile/src/components/atoms/icon/Share.tsx
@@ -1,45 +1,46 @@
-type Props = {
-  width: string;
-  height: string;
-  color: string;
-};
+import { IconProps } from "src/type/props";
 
-function Share({ color, width, height }: Props) {
+function Share({ size, stroke }: IconProps) {
   return (
     <svg
-      width={width}
-      height={height}
-      viewBox="0 0 14 14"
+      width={size}
+      height="auto"
+      viewBox="0 0 22 22"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M2.3582 9.03281C3.20513 9.03281 3.8917 8.34624 3.8917 7.49932C3.8917 6.65239 3.20513 5.96582 2.3582 5.96582C1.51128 5.96582 0.824707 6.65239 0.824707 7.49932C0.824707 8.34624 1.51128 9.03281 2.3582 9.03281Z"
-        stroke={color}
+        d="M3.6283 13.7821C5.07798 13.7821 6.25318 12.6378 6.25318 11.2262C6.25318 9.81469 5.07798 8.67041 3.6283 8.67041C2.17862 8.67041 1.00342 9.81469 1.00342 11.2262C1.00342 12.6378 2.17862 13.7821 3.6283 13.7821Z"
+        stroke={stroke}
+        strokeWidth="1.2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M10.9754 4.43052C11.8223 4.43052 12.5089 3.74395 12.5089 2.89702C12.5089 2.0501 11.8223 1.36353 10.9754 1.36353C10.1285 1.36353 9.44189 2.0501 9.44189 2.89702C9.44189 3.74395 10.1285 4.43052 10.9754 4.43052Z"
-        stroke={color}
+        d="M18.3783 6.11165C19.828 6.11165 21.0032 4.96737 21.0032 3.55583C21.0032 2.14428 19.828 1 18.3783 1C16.9286 1 15.7534 2.14428 15.7534 3.55583C15.7534 4.96737 16.9286 6.11165 18.3783 6.11165Z"
+        stroke={stroke}
+        strokeWidth="1.2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M10.9754 13.3634C11.8223 13.3634 12.5089 12.6768 12.5089 11.8299C12.5089 10.983 11.8223 10.2964 10.9754 10.2964C10.1285 10.2964 9.44189 10.983 9.44189 11.8299C9.44189 12.6768 10.1285 13.3634 10.9754 13.3634Z"
-        stroke={color}
+        d="M18.3783 20.9998C19.828 20.9998 21.0032 19.8556 21.0032 18.444C21.0032 17.0325 19.828 15.8882 18.3783 15.8882C16.9286 15.8882 15.7534 17.0325 15.7534 18.444C15.7534 19.8556 16.9286 20.9998 18.3783 20.9998Z"
+        stroke={stroke}
+        strokeWidth="1.2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M9.57676 3.80005L4.07324 7.04783"
-        stroke={color}
+        d="M15.9843 5.06079L6.56396 10.4738"
+        stroke={stroke}
+        strokeWidth="1.2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M3.69434 8.23438L9.30614 11.3794"
-        stroke={color}
+        d="M5.91553 12.4514L15.5212 17.6931"
+        stroke={stroke}
+        strokeWidth="1.2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />

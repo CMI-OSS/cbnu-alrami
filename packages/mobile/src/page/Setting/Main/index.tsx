9cc2f64e9025ade0dfb1dc8baffc7872e1413261
동동이 <drdragon777@naver.com>
2022-08-10 02:55:49 +0900

    refactor: settingConfig data

--- packages/mobile/src/page/Setting/Main/index.tsx
+++ packages/mobile/src/page/Setting/Main/index.tsx
@@ -1,27 +1,26 @@
-import { settingConfig } from "src/__mocks__";
 import SettingContact from "src/page/Setting/Contact";
 import SettingMenu from "src/page/Setting/Menu";
 import SettingTemplate from "src/page/Setting/SettingTemplate";
 import { useAppSelector } from "src/store";
 
-import { settingMenuList } from "./constants";
+import { settingConfig, settingMenuList } from "./constants";
 import $ from "./style.module.scss";
 
 function Main() {
+  const representativeCafeteria =
+    localStorage.getItem("representativeCafeteria") || "설정 안함";
   const { isDisplayContact } = useAppSelector((state) => {
     return state.settingReducer.setting;
   });
+  const settingData = settingConfig(representativeCafeteria);
 
   return (
     <SettingTemplate title="설정" className={$["setting-main"]}>
       {settingMenuList.map((route) => {
-        return (
-          <SettingMenu
-            key={route.label}
-            route={route}
-            config={settingConfig[route.label]}
-          />
-        );
+        const candidate = settingData[route.label];
+        const config = candidate === "표시 안함" ? "설정 안함" : candidate;
+
+        return <SettingMenu key={route.label} {...{ route, config }} />;
       })}
       {isDisplayContact && <SettingContact />}
     </SettingTemplate>

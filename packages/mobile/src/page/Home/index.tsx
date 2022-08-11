73699ca569fd6a36313e44df662d27e29e288bfa
Hyuno Choi <soonitoon@gmail.com>
2022-08-11 23:12:41 +0900

    chore: Remove unnecessary code

--- packages/mobile/src/page/Home/index.tsx
+++ packages/mobile/src/page/Home/index.tsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 import Footer from "@components/molecules/Footer";
 import dayjs from "dayjs";
-import { usePopularArticles } from "src/api/article";
 import { useSchedule } from "src/api/schedule";
 import { Setting } from "src/components/atoms/icon";
 import Weather from "src/page/Home/Weather";
@@ -15,23 +14,14 @@ import Schedule from "./Schedule";
 import $ from "./style.module.scss";
 
 function Home() {
-  const today = dayjs();
   const [ uuid, setUuid ] = useState("");
   const onMessageHandler = (e: any) => {
     const event = JSON.parse(e.data);
     setUuid(e.data); // Todo: uuid 보내는 api 연결
     localStorage.setItem("item", JSON.stringify(event));
   };
-  const {
-    data: popularArticleData,
-    isLoading: popularArticleLoading,
-    isError: popularArticleError,
-  } = usePopularArticles();
-  const {
-    data: scheduleData,
-    isLoading: scheduleLoading,
-    isError: scheduleError,
-  } = useSchedule(today.format("YYYY-MM-DD"));
+  const today = dayjs();
+  const { data: scheduleData } = useSchedule(today.format("YYYY-MM-DD"));
 
   useEffect(() => {
     if (window.ReactNativeWebView) {
@@ -44,30 +34,19 @@ function Home() {
     }
   }, [ uuid ]);
 
-  if (popularArticleLoading || scheduleLoading) return <div>로딩중입니다.</div>;
-  if (popularArticleError || scheduleError)
-    return <div>에러가 발생했습니다.</div>;
-  if (scheduleData === undefined) return <div>일정 불러오기 실패</div>;
-
-  const popularNotifications = popularArticleData!.data;
-  // TODO: 백엔드 api 확인 후 수정
-  const schedules = scheduleData.data;
-
-  const lastestNotifications = [ "최신1", "최신2", "최신3", "최신4", "최신5" ];
-
   return (
     <section className={$.home}>
       <header className={$.header}>
         <div className={$["header-content"]}>
           <h1 className={$.title}>충림이</h1>
-          <p>오늘은 총 {schedules.length}개의 일정이 있어요</p>
+          <p>오늘은 총 {scheduleData?.data.length}개의 일정이 있어요</p>
         </div>
         <Link to="/setting">
           <Setting size={24} stroke="#aaa" />
         </Link>
       </header>
       <div className={$.schedule}>
-        {schedules.map(({ id, content, startDate, endDate }) => {
+        {scheduleData?.data.map(({ id, content, startDate, endDate }) => {
           return (
             <Schedule key={id} {...{ content, startDate, endDate, today }} />
           );

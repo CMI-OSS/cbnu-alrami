740b14a1e08db31aa868013b3ee5b9d0035517f3
HyunoChoi <soonitoon@gmail.com>
2022-08-13 16:40:43 +0900

    [mobile] 홈 일정 카드 API 연동 (#438)
    * feat: Connect schedules API data
    
    * fix: Add margin-top based on design guide
    
    * chore: Remove unnecessary code

--- packages/mobile/src/page/Home/index.tsx
+++ packages/mobile/src/page/Home/index.tsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 import Footer from "@components/molecules/Footer";
 import dayjs from "dayjs";
-import { COLLEGE_SCHEDULES } from "src/__mocks__/schedules";
 import { useSchedule } from "src/api/schedule";
 import { Setting } from "src/components/atoms/icon";
 import Weather from "src/page/Home/Weather";
@@ -15,7 +14,6 @@ import Schedule from "./Schedule";
 import $ from "./style.module.scss";
 
 function Home() {
-  const { data } = useSchedule();
   const [ uuid, setUuid ] = useState("");
   const onMessageHandler = (e: any) => {
     const event = JSON.parse(e.data);
@@ -23,6 +21,7 @@ function Home() {
     localStorage.setItem("item", JSON.stringify(event));
   };
   const today = dayjs();
+  const { data: scheduleData } = useSchedule(today.format("YYYY-MM-DD"));
 
   useEffect(() => {
     if (window.ReactNativeWebView) {
@@ -40,14 +39,14 @@ function Home() {
       <header className={$.header}>
         <div className={$["header-content"]}>
           <h1 className={$.title}>충림이</h1>
-          <p>오늘은 총 {COLLEGE_SCHEDULES.length}개의 일정이 있어요</p>
+          <p>오늘은 총 {scheduleData?.data.length}개의 일정이 있어요</p>
         </div>
         <Link to="/setting">
           <Setting size={24} stroke="#aaa" />
         </Link>
       </header>
       <div className={$.schedule}>
-        {COLLEGE_SCHEDULES.map(({ id, content, startDate, endDate }) => {
+        {scheduleData?.data.map(({ id, content, startDate, endDate }) => {
           return (
             <Schedule key={id} {...{ content, startDate, endDate, today }} />
           );

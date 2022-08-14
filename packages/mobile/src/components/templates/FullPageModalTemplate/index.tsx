064f23e0c97f86ad92b202f92698b789f0afedc6
nohgijin <ynda11111@naver.com>
2022-08-14 23:24:14 +0900

    chore: console 제거

--- packages/mobile/src/components/templates/FullPageModalTemplate/index.tsx
+++ packages/mobile/src/components/templates/FullPageModalTemplate/index.tsx
@@ -13,8 +13,6 @@ type Props = {
 function FullPageModalTemplate({ left, title, right, children }: Props) {
   const navigate = useNavigate();
 
-  console.log("title", title);
-
   return (
     <div className={$["full-modal"]}>
       <div className={$.header}>

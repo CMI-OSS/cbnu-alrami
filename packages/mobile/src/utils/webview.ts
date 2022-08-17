15a0f705e59532cb45df78ee799e392c761dbff9
nohgijin <ynda11111@naver.com>
2022-08-18 08:34:43 +0900

    refactor: process 환경변수 이용하여 dev모드 판별

--- packages/mobile/src/utils/webview.ts
+++ packages/mobile/src/utils/webview.ts
@@ -1,9 +1,8 @@
 import { getUA, isMobile } from "react-device-detect";
 
 const CBNU_ALRAMI_APP_USER_AGENT = "cbnu-alrami-app";
-const LOCALHOST = "http://localhost:3000/";
 
 const isWebView = isMobile && getUA.indexOf(CBNU_ALRAMI_APP_USER_AGENT) > -1;
-const isDev = window.location.href.indexOf(LOCALHOST) === 0;
+const isDev = process.env.NODE_ENV === "development";
 
 export { isWebView, isDev };

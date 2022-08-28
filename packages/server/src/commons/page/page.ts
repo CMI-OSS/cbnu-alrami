351826474d6decc0093464166ead7a66904cd3f6
우디 <38103085+EunjiShin@users.noreply.github.com>
2022-08-28 21:48:41 +0900

    [server] pagination response 수정 (#505)
    * :sparkles: 전체 페이지 수, 전체 아이템 수 필드 추가
    
    * :sparkles: 페이징 결과 없을 때 빈 리스트 리턴하도록 exception 삭제

--- packages/server/src/commons/page/page.ts
+++ packages/server/src/commons/page/page.ts
@@ -3,9 +3,14 @@ import { PageRequest } from "./page.request";
 export class Page {
   // 마지막 페이지 확인 플래그
   isEnd: boolean;
+  totalItemCount: number;
+  totalPageCount: number;
+
   constructor(pageRequest: PageRequest, totalItemCount: number) {
     const currentPageNo = pageRequest.pageNo;
     const totalPageCount = Math.ceil(totalItemCount / pageRequest.pageSize);
-    this.isEnd = currentPageNo === totalPageCount;
+    this.isEnd = currentPageNo >= totalPageCount;
+    this.totalItemCount = totalItemCount;
+    this.totalPageCount = totalPageCount;
   }
 }

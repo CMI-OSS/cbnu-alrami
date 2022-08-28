f7e2ffada4605af6b220d260292268b2f21fb20e
동동이 <drdragon777@naver.com>
2022-08-29 02:11:24 +0900

    feat: add LoadingSpinner component

---
+++ packages/mobile/src/components/atoms/LoadingSpinner/index.tsx
@@ -0,0 +1,25 @@
+import $ from "./style.module.scss";
+
+interface Props {
+  width: number;
+  borderWidth: number;
+  color: string;
+}
+
+export default function LoadingSpinner({ width, borderWidth, color }: Props) {
+  return (
+    <div className={$.loading}>
+      <div
+        style={{
+          width: `${width}rem`,
+          height: `${width}rem`,
+          borderWidth: `${borderWidth}rem`,
+          borderTopColor: color,
+          borderRightColor: color,
+          borderBottomColor: color,
+        }}
+        className={$.spinner}
+      ></div>
+    </div>
+  );
+}

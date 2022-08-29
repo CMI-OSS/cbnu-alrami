b18d043eddc219ba6a88a3b358d9c47af2362467
우디 <38103085+EunjiShin@users.noreply.github.com>
2022-08-29 17:55:20 +0900

    :sparkles: 현재 페이지 인덱스 res 추가 (#511)

--- packages/server/src/commons/page/page.ts
+++ packages/server/src/commons/page/page.ts
@@ -3,6 +3,7 @@ import { PageRequest } from "./page.request";
 export class Page {
   // 마지막 페이지 확인 플래그
   isEnd: boolean;
+  pageNumber: number;
   totalItemCount: number;
   totalPageCount: number;
 
@@ -10,6 +11,7 @@ export class Page {
     const currentPageNo = pageRequest.pageNo;
     const totalPageCount = Math.ceil(totalItemCount / pageRequest.pageSize);
     this.isEnd = currentPageNo >= totalPageCount;
+    this.pageNumber = pageRequest.pageNo;
     this.totalItemCount = totalItemCount;
     this.totalPageCount = totalPageCount;
   }

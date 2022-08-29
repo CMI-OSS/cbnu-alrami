42015897092ff19cb5bfad425c6d30b50c087393
Hyuno Choi <soonitoon@gmail.com>
2022-08-29 23:52:03 +0900

    fix: Make Greeting card width 100% of parent element

--- packages/mobile/src/page/Home/Restaurant/Greeting/index.tsx
+++ packages/mobile/src/page/Home/Restaurant/Greeting/index.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import BorderBox from "src/components/atoms/BorderBox";
 
 import $ from "./style.module.scss";
@@ -11,10 +12,11 @@ function Greeting({ onClick, className }: Props) {
   return (
     <button
       type="button"
-      {...{ className, onClick }}
       aira-label="대표 식당 선택하기"
+      onClick={onClick}
+      className={classNames(className, $.button)}
     >
-      <BorderBox height={188} width={312} className={$.box}>
+      <BorderBox height={188} className={$.box}>
         <span className={$.message}>
           화면을 눌러 홈화면에 표시할 식당을
           <br /> 선택해주세요

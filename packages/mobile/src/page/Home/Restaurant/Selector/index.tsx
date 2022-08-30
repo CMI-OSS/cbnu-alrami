7cf46ed6f3e948eb3443e8435821131e1ace17f6
Hyuno Choi <soonitoon@gmail.com>
2022-08-30 13:22:49 +0900

    feat: Make Home cafeteria selector to dimmed modal

--- packages/mobile/src/page/Home/Restaurant/Selector/index.tsx
+++ packages/mobile/src/page/Home/Restaurant/Selector/index.tsx
@@ -24,32 +24,38 @@ function Selector({
   className,
 }: Props) {
   return (
-    <BorderBox height="auto" className={classNames($.selector, className)}>
-      <span className={$["selector-title"]}>식당 선택</span>
-      <ul>
-        {SELECTOR_ITEMS.map(({ name, id }) => {
-          return (
-            <li key={id} className={$.item}>
-              <button
-                type="button"
-                className={classNames($.button, {
-                  [$.selected]: name === cafeteriaName,
-                })}
-                onClick={() => {
-                  return onCafeteriaSelect(name as Restaurant);
-                }}
-                aria-label={`${name} 선택하기`}
-              >
-                {name}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-      <button type="button" className={$.cancel} onClick={onSelectorCancel}>
-        취소
-      </button>
-    </BorderBox>
+    <div className={$["dimmed-box"]}>
+      <BorderBox
+        height="auto"
+        width={328}
+        className={classNames($.selector, className)}
+      >
+        <span className={$["selector-title"]}>식당 선택</span>
+        <ul>
+          {SELECTOR_ITEMS.map(({ name, id }) => {
+            return (
+              <li key={id} className={$.item}>
+                <button
+                  type="button"
+                  className={classNames($.button, {
+                    [$.selected]: name === cafeteriaName,
+                  })}
+                  onClick={() => {
+                    return onCafeteriaSelect(name as Restaurant);
+                  }}
+                  aria-label={`${name} 선택하기`}
+                >
+                  {name}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <button type="button" className={$.cancel} onClick={onSelectorCancel}>
+          취소
+        </button>
+      </BorderBox>
+    </div>
   );
 }
 

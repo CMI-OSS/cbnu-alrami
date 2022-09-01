f1117b212830e996f657de323d0392e6ea2056b8
HyunoChoi <soonitoon@gmail.com>
2022-09-01 22:11:31 +0900

    feat: Apply new-designed Arrow icon (#522)

--- packages/mobile/src/components/atoms/icon/Arrow.tsx
+++ packages/mobile/src/components/atoms/icon/Arrow.tsx
@@ -12,13 +12,14 @@ function Arrow({ size, stroke, style }: Props) {
       width={size}
       height="100%"
       style={style}
-      viewBox="0 0 7 14"
+      viewBox="0 0 8 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M6.25342 13.3633L1.00342 7.36328L6.25342 1.36328"
+        d="M7.00342 17L1.00342 9L7.00342 1"
         stroke={stroke}
+        strokeWidth="1.2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />

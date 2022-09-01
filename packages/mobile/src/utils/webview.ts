92e98265814c882a4f2147bb483a74cb7f699364
thdwlsgus0 <thdwlsgus0@naver.com>
2022-09-01 17:14:06 +0900

    refactor: webview 판단 로직 수정

--- packages/mobile/src/utils/webview.ts
+++ packages/mobile/src/utils/webview.ts
@@ -1,10 +1,14 @@
-import { getUA, isIOS, isMobile } from "react-device-detect";
+import { getUA, isMobile } from "react-device-detect";
 
-const CBNU_ALRAMI_APP_USER_AGENT = isIOS
-  ? "cbnu_alrami_ios"
-  : "cbnu_alrami_android";
+const CBNU_ALRAMI_IOS_USER_AGENT = "cbnu_alrami_ios";
+const CBNU_ALRAMI_ANDROID_USER_AGENT = "cbnu_alrami_android";
 
-const isWebView = isMobile && getUA.indexOf(CBNU_ALRAMI_APP_USER_AGENT) > -1;
+const isFromIosApp = getUA.includes(CBNU_ALRAMI_IOS_USER_AGENT);
+const isFromAndroidApp = getUA.includes(CBNU_ALRAMI_ANDROID_USER_AGENT);
+
+const isFromApp = isFromIosApp || isFromAndroidApp;
+
+const isWebView = isMobile && isFromApp;
 const isDev = process.env.NODE_ENV === "development";
 
 export { isWebView, isDev };

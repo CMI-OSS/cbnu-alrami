974f8e19989e273e4f436a375afa4b9fddb100f5
동동이 <drdragon777@naver.com>
2022-09-03 18:44:30 +0900

    feat : add creator page

--- packages/mobile/src/page/Setting/index.tsx
+++ packages/mobile/src/page/Setting/index.tsx
@@ -4,12 +4,15 @@ import Cafeteria from "src/page/Setting/Cafeteria";
 import Main from "src/page/Setting/Main";
 import Subscription from "src/page/Setting/Subscription";
 
+import Creator from "./Creator";
+
 function SettingRoute() {
   return (
     <Routes>
       <Route index element={<Main />} />
       <Route path="/cafeteria" element={<Cafeteria />} />
       <Route path="/subscription" element={<Subscription />} />
+      <Route path="/creator" element={<Creator />} />
     </Routes>
   );
 }

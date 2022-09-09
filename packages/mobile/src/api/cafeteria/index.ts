6279c4b87660f1df37320fac68acf5d2f2d5a7db
동동이 <drdragon777@naver.com>
2022-09-09 19:59:34 +0900

    chore : delete error test code

--- packages/mobile/src/api/cafeteria/index.ts
+++ packages/mobile/src/api/cafeteria/index.ts
@@ -1,6 +1,5 @@
 import http from "src/api/core";
 
-let first = true;
 export const getCafeteria = async (
   params: req.Cafeteria,
 ): Promise<res.Cafeteria[]> => {
@@ -8,11 +7,6 @@ export const getCafeteria = async (
     const response = await http.get(
       `/cafeterias/${params.cafeteriaId}/menus?date=${params.date}`,
     );
-    if (first) {
-      // error test
-      first = false;
-      throw Error("오류가 발생했습니다.");
-    }
     return response;
   } catch (err) {
     throw new Error("네트워크 오류가 발생했습니다.<br>다시 시도해주세요.");

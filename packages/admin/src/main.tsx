27c1210f31eedd366e7f80b8952e5a0d91fa4b09
jess <jaryapp@gmail.com>
2022-09-25 14:05:10 +0900

    ✨ Recoil 추가

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { RecoilRoot } from "recoil";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <RecoilRoot>
     <App />
-  </React.StrictMode>,
+  </RecoilRoot>,
 );

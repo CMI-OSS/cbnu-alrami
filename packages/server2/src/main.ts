ea0e56aa76388adc66dd0ed76980d66fc623bf2f
jess <jaryapp@gmail.com>
2022-11-06 06:58:44 +0900

    ✨ article 기본 셋업 및 스웨거 설정

--- packages/server2/src/main.ts
+++ packages/server2/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  SwaggerConfig(app);
+
   await app.listen(3000);
 }
 bootstrap();

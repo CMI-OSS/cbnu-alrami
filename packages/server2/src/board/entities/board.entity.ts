2bfdb6fe571fdfb1ab77e7def1b8014e63094b67
jess <jaryapp@gmail.com>
2022-11-10 06:04:58 +0900

    ✨ board 엔티티 추가

--- packages/server2/src/board/entities/board.entity.ts
+++ packages/server2/src/board/entities/board.entity.ts
@@ -1,5 +1,18 @@
 import { UpdatableCommonEntity } from "src/common/entity";
-import { Entity } from "typeorm";
+import { Column, Entity, Tree, TreeChildren, TreeParent } from "typeorm";
 
 @Entity()
-export class Board extends UpdatableCommonEntity {}
+@Tree("materialized-path")
+export class Board extends UpdatableCommonEntity {
+  @Column({ type: "varchar", length: 100 })
+  name: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  url?: string;
+
+  @TreeParent()
+  parent: Board;
+
+  @TreeChildren()
+  children: Board[];
+}

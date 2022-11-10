c76e9617c392275cccea2181ff642a791c2b8034
동동이 <drdragon777@naver.com>
2022-11-10 23:01:20 +0900

    feat - add PlaceList view

--- packages/admin/src/pages/PlaceManagementPage/PlaceList/PlaceList.tsx
+++ packages/admin/src/pages/PlaceManagementPage/PlaceList/PlaceList.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import PaginationView from "src/components/Pagination/Pagination.view";
-import { getArticles } from "src/newApi/articleApi/getArticles";
+import { getPlaces } from "src/newApi/placeApi/getPlaces";
 import { isOutputType } from "src/newApi/types";
 
 import $ from "./PlaceList.module.scss";
@@ -14,14 +14,11 @@ export default function PlaceList() {
   const page = Number(params.get("page")) || 1;
   const navigate = useNavigate();
 
-  const pageSize = 2;
-
-  const { data: articlePageOutput, isLoading } = useQuery(
-    [ "articles", 30101, page, pageSize ],
-    () => getArticles({ boardId: 30101, pageNo: page, pageSize }),
+  const { data: placePageOutput, isLoading } = useQuery([ "places" ], () =>
+    getPlaces(),
   );
 
-  if (isLoading || !articlePageOutput) return null;
+  if (isLoading || !placePageOutput) return null;
 
   const handleClickArticle = (articleId: number) => {
     navigate(`/board/articles/${articleId}`);
@@ -39,20 +36,21 @@ export default function PlaceList() {
     navigate(`?page=${page + 1}`);
   };
 
-  if (!isOutputType(articlePageOutput, "GetArticlesApiOutput_Success"))
-    return null;
+  if (!isOutputType(placePageOutput, "GetPlacesApiOutput_Success")) return null;
+
+  const count = 7;
+  const places =
+    placePageOutput?.content.slice((page - 1) * count, page * count) || [];
+  const totalPage = Math.ceil(placePageOutput.content.length / count) || 0;
 
   return (
     <div className={$["article-list"]}>
       <div>건물 목록</div>
-      <PlaceListView
-        articles={articlePageOutput?.contents}
-        onClickArticle={handleClickArticle}
-      />
+      <PlaceListView places={places} onClickPlace={handleClickArticle} />
       <PaginationView
         currentPage={page}
-        displayPageCount={10}
-        totalPage={articlePageOutput.pagination.totalPageCount}
+        displayPageCount={count}
+        totalPage={totalPage}
         onClick={handleClickPage}
         onClickNext={handleClickNext}
         onClickPrev={handleClickPrev}
--- packages/admin/src/pages/PlaceManagementPage/PlaceList/PlaceList.view.tsx
+++ packages/admin/src/pages/PlaceManagementPage/PlaceList/PlaceList.view.tsx
@@ -2,43 +2,41 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
 import classnames from "classnames";
-import dayjs from "dayjs";
-import { Article } from "src/api/types";
+import { PlaceListItem } from "src/newApi/placeApi/getPlaces";
 
 import $ from "./PlaceList.module.scss";
 
 interface Props {
-  articles: Article[];
-  onClickArticle: (articleId: number) => void;
+  places: PlaceListItem[];
+  onClickPlace: (placeId: number) => void;
 }
 
-export default function PlaceListView({ articles, onClickArticle }: Props) {
+export default function PlaceListView({ places, onClickPlace }: Props) {
   return (
     <div className={$.table}>
       <div className={classnames($.row, $.header)}>
         <div className={classnames($.cell, $.id)}>ID</div>
-        <div className={classnames($.cell, $.title)}>제목</div>
-        <div className={classnames($.cell, $.date)}>작성일</div>
-        <div className={classnames($.cell, $.id)}>조회수</div>
-        <div className={classnames($.cell, $.scraps)}>스크랩</div>
-        <div className={classnames($.cell, $.id)}>수정일</div>
+        <div className={classnames($.cell, $.title)}>이름</div>
+        <div className={classnames($.cell, $.date)}>위도</div>
+        <div className={classnames($.cell, $.id)}>경도</div>
+        <div className={classnames($.cell, $.scraps)}>주소</div>
+        <div className={classnames($.cell, $.id)}>건물번호</div>
       </div>
-      {articles.map((article) => (
+      {places.map((place) => (
         <div
           className={$.row}
-          key={article.id}
-          onClick={() => onClickArticle(article.id)}
+          key={place.id}
+          onClick={() => onClickPlace(place.id)}
         >
-          <div className={classnames($.cell, $.id)}>{article.id}</div>
-          <div className={classnames($.cell, $.title)}>{article.title}</div>
+          <div className={classnames($.cell, $.id)}>{place.id}</div>
+          <div className={classnames($.cell, $.title)}>{place.name}</div>
 
-          <div className={classnames($.cell, $.date)}>
-            {dayjs(article.date).format("YYYY.MM.DD")}
-          </div>
-          <div className={classnames($.cell, $.hits)}>{article.hits}</div>
-          <div className={classnames($.cell, $.scraps)}>{article.scraps}</div>
-          <div className={classnames($.cell, $.date)}>
-            {dayjs(article.updatedAt).format("YYYY.MM.DD HH:MM")}
+          <div className={classnames($.cell, $.hits)}>{place.latitude}</div>
+          <div className={classnames($.cell, $.scraps)}>{place.longtitude}</div>
+
+          <div className={classnames($.cell, $.scraps)}>{place.address}</div>
+          <div className={classnames($.cell, $.scraps)}>
+            {place.school?.area}
           </div>
         </div>
       ))}

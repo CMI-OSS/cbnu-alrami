aeb7b72749b595ab75e5262131984a62aa99cef4
nohgijin <ynda11111@naver.com>
2022-11-12 11:51:58 +0900

    chore: 목데이터 제거

--- packages/mobile/src/page/Notice/Detail/index.tsx
+++ packages/mobile/src/page/Notice/Detail/index.tsx
@@ -12,29 +12,6 @@ import { isWebView } from "src/utils/webview";
 import Footer from "./Footer";
 import $ from "./style.module.scss";
 
-const IMAGES = [
-  {
-    id: 531,
-    url: "https://user-images.githubusercontent.com/34129711/147549612-edea062f-1c2b-4049-bc48-49900be89826.jpg",
-  },
-  {
-    id: 532,
-    url: "https://user-images.githubusercontent.com/34129711/147549639-c30f6506-8fac-4bf2-84a5-c5de065cf26b.jpg",
-  },
-  {
-    id: 533,
-    url: "https://user-images.githubusercontent.com/34129711/176329367-b87f2b91-a955-4eca-8537-6ec44bc10b9a.jpg",
-  },
-  {
-    id: 534,
-    url: "https://user-images.githubusercontent.com/34129711/176329372-fb5d8f2f-fc97-408c-b2dd-e932e535fa66.jpg",
-  },
-  {
-    id: 535,
-    url: "https://user-images.githubusercontent.com/34129711/176329355-55837191-7c11-4f9e-8c44-75113fbfdd0c.jpg",
-  },
-];
-
 function Detail() {
   const { articleId } = useParams();
   const {
@@ -69,10 +46,9 @@ function Detail() {
               <Slider
                 className={$["article-image-wrapper"]}
                 {...{ order, setOrder }}
-                total={IMAGES.length}
+                total={article.images.length}
               >
-                {/* {article.images.map((image) => { */}
-                {IMAGES.map((image) => {
+                {article.images.map((image) => {
                   return (
                     <Image
                       className={$["article-image"]}

01cb732e18c2895f420be375cb815bb79684879f
thdwlsgus0 <thdwlsgus0@naver.com>
2023-01-15 15:15:02 +0900

    refactor: env 제거

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -2,24 +2,25 @@ import * as path from "path";
 
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-const env = loadEnv('analyze', process.cwd(), '');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 8000,
-  },
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "./src"),
-      "@shared": path.resolve(__dirname, "../shared/src"),
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      port: 8000,
     },
-  },
-  plugins: [ react(), env.VITE_ANALYZE? visualizer({
-    filename: "./stat.html",
-    open: true,
-    brotliSize: true,
-  }): [] ],
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "./src"),
+        "@shared": path.resolve(__dirname, "../shared/src"),
+      },
+    },
+    plugins: [ react(), mode === 'analyze'? visualizer({
+      filename: "./stat.html",
+      open: true,
+      brotliSize: true,
+    }): [] ],
+  }
 });

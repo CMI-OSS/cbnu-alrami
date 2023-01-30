418b8da53e5f0a0dac0d405adff232cae8bd692d
jess <jaryapp@gmail.com>
2023-01-30 09:46:33 +0900

    ✨ 서버 URL 수정

--- packages/shared/src/constant.ts
+++ packages/shared/src/constant.ts
@@ -1,4 +1,4 @@
 export const apiServer = {
-  local: "http://localhost:3001/api-docs-json",
-  dev: "https://dev-server2.cmi.kro.kr/api-docs-json",
+  local: "http://localhost:3001",
+  dev: "https://dev-server2.cmi.kro.kr",
 };

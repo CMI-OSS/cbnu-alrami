00019a66a3f54b3804ea281e152d85c0c048a268
jess <jaryapp@gmail.com>
2023-02-13 07:41:14 +0900

    ✨ BASE_URL 설정

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+import { apiServer } from "@shared/constant";
+import { OpenAPI } from "@shared/swagger-api/generated/core/OpenAPI";
 import { RecoilRoot } from "recoil";
 
 import App from "./App";
 
 const queryClient = new QueryClient();
+OpenAPI.BASE = apiServer.local;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>

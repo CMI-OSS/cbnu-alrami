86e8aaa0f44300f2055f2d31cf45292a5205ab4b
nohgijin <ynda11111@naver.com>
2023-02-15 01:01:32 +0900

    feat: 인기,최신,북마크 공지사항 api 연결

--- packages/mobile/src/page/Article/components/ArticleList/index.tsx
+++ packages/mobile/src/page/Article/components/ArticleList/index.tsx
@@ -1,8 +1,51 @@
+import { useLocation } from "react-router-dom";
+
+import {
+  useBookmarkArticlesQuery,
+  usePopularArticlesQuery,
+  useSubscribeArticlesQuery,
+} from "@hooks/api/article1";
+import { useBoardArticlesQuery } from "@hooks/api/board1";
+import { useIntersect } from "@hooks/UseIntersect";
 import ArticleItem from "src/page/Article/components/ArticleItem";
 
 import $ from "./style.module.scss";
 
+const useArticles = () => {
+  const { pathname } = useLocation();
+  const kind = pathname.split("/").at(-1);
+
+  if (kind === "popular") {
+    return usePopularArticlesQuery();
+  }
+
+  if (kind === "subscribe") {
+    return useSubscribeArticlesQuery();
+  }
+
+  if (kind === "bookmark") {
+    return useBookmarkArticlesQuery();
+  }
+  return useBoardArticlesQuery({ id: 1010101 });
+};
+
 function ArticleList() {
+  const {
+    data: articlesData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = useArticles();
+
+  const ref = useIntersect(async (entry, observer) => {
+    observer.unobserve(entry.target);
+    if (hasNextPage && !isFetching) {
+      await fetchNextPage();
+    }
+  });
+
+  console.log(articlesData, "데이터임니다");
+
   return (
     <div className={$["article-list"]}>
       <ArticleItem />
@@ -12,6 +55,7 @@ function ArticleList() {
       <ArticleItem />
       <ArticleItem />
       <ArticleItem />
+      <div ref={ref} />
     </div>
   );
 }

6dc50d378809bf36a7735bbaa3f788df2988a6c4
nohgijin <ynda11111@naver.com>
2023-02-15 23:03:12 +0900

    feat: 공지사항 디테일 푸터 추가

--- packages/mobile/src/page/Article/components/Footer/index.tsx
+++ packages/mobile/src/page/Article/components/Footer/index.tsx
@@ -1,7 +1,15 @@
+import { Internet, Share, Star } from "@components/atoms/icon";
+
 import $ from "./style.module.scss";
 
 function ArticleFooter() {
-  return <div className={$["article-footer"]}></div>;
+  return (
+    <div className={$["article-footer"]}>
+      <Share size={24} stroke="#AAAAAA" />
+      <Star size={24} stroke="#AAAAAA" />
+      <Internet size={24} stroke="#AAAAAA" />
+    </div>
+  );
 }
 
 export default ArticleFooter;

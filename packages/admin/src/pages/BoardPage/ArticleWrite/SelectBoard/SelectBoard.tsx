d76614370a3ed6a79cfc99cfe68b20ce3bd8f46c
jess <jaryapp@gmail.com>
2023-02-15 15:19:59 +0900

    ✨ 권한이 있는 게시판 불러오기

--- packages/admin/src/pages/BoardPage/ArticleWrite/SelectBoard/SelectBoard.tsx
+++ packages/admin/src/pages/BoardPage/ArticleWrite/SelectBoard/SelectBoard.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
-import { useGetAuthorityBoardsQuery } from "src/api/board";
+import { AdminApiService } from "@shared/swagger-api/generated/services/AdminApiService";
 
 import SelectBoardView, { Props as ViewProps } from "./SelectBoard.view";
 
 export default function SelectBoard() {
   const [ selectedBoardId, setSelectedBoardId ] = useState("1");
 
-  const { data: authorityBoards, isLoading } = useGetAuthorityBoardsQuery();
-
+  const { data: authorityBoards, isLoading } = useQuery(
+    [ "boardAuthority" ],
+    () => AdminApiService.adminControllerGetAuthorityBoards(),
+  );
   if (isLoading || !authorityBoards) return null;
 
   const viewProps: ViewProps = {
     options: authorityBoards?.map((authorityBoard) => ({
       value: authorityBoard.id,
-      text: `${authorityBoard.parent?.name} - ${authorityBoard.name}`,
+      text: `${authorityBoard.board.parent?.name} - ${authorityBoard.board.name}`,
     })),
 
     value: selectedBoardId,

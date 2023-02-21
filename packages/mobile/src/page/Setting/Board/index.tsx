6679f401767ef508bb8068ecfa2c39f24518e25f
nohgijin <ynda11111@naver.com>
2023-02-21 16:50:46 +0900

    refactor: 구독중인 공지사항 없을 경우 이미지 추가

--- packages/mobile/src/page/Setting/Board/index.tsx
+++ packages/mobile/src/page/Setting/Board/index.tsx
@@ -4,6 +4,7 @@ import { Plus } from "@components/atoms/icon";
 import SubscriptionNoticeGroup from "@components/shared/SubscriptionNoticeGroup";
 import { useSubscribeBoardsQuery } from "@hooks/api/board";
 import { useSetRecoilState } from "recoil";
+import guideEmptySubscriptionSetting from "src/assets/guide_empty_subscription_setting.png";
 import SettingTemplate from "src/page/Setting/SettingTemplate";
 import { boardOriginStatus } from "src/states";
 
@@ -16,6 +17,27 @@ function Board() {
   const handlePlusClick = () => {
     setBoardOrigin("setting");
   };
+
+  if (!subscribeBoardsData?.length) {
+    return (
+      <SettingTemplate
+        title="구독/알림"
+        right={
+          <Link to="/board" onClick={handlePlusClick}>
+            <Plus size={16} stroke="#AAAAAA" />
+          </Link>
+        }
+      >
+        <div className={$["empty-subscription"]}>
+          <img
+            src={guideEmptySubscriptionSetting}
+            width={283}
+            alt="구독중인 공지사항 없음"
+          />
+        </div>
+      </SettingTemplate>
+    );
+  }
 
   return (
     <SettingTemplate

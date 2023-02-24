685eb5762ed7a35648aaa5f20a7adf2d2663e3a2
thdwlsgus0 <thdwlsgus0@naver.com>
2023-02-25 00:52:04 +0900

    refactor: areaQuery와 schoolsquery 합침

--- packages/mobile/src/hooks/api/school/index.ts
+++ packages/mobile/src/hooks/api/school/index.ts
@@ -3,9 +3,11 @@ import { PlaceApiService } from "@shared/swagger-api/generated";
 import { queryKey } from "src/consts/react-query";
 import { GetParams } from "src/type/utils";
 
-export const useSchoolsQuery = () => {
-  return useCoreQuery(queryKey.schools, () => {
-    return PlaceApiService.placeControllerFindSchool({})
+export const useSchoolQuery = (params?: GetParams<typeof PlaceApiService.placeControllerFindSchool>) => {
+  return useCoreQuery(queryKey.schools({...params}), () => {
+    return PlaceApiService.placeControllerFindSchool({
+      ...params
+    })
   })
 };
 
@@ -16,11 +18,3 @@ export const useSchoolQuery = (placeId: GetParams<typeof PlaceApiService.placeCo
     })
   });
 };
-
-export const useSchoolAreaQuery = (position: GetParams<typeof PlaceApiService.placeControllerFindSchool>["area"]) => {
-  return useCoreQuery(queryKey.schools, () => {
-    return PlaceApiService.placeControllerFindSchool({
-      area: position
-    })
-  })
-}

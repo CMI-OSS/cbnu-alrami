8bb4ec3fbb54bde713c090c83764ced0b1ecb86b
jess <jaryapp@gmail.com>
2023-03-04 16:21:09 +0900

    ✨ 게시판 리스트 보여주기

--- packages/admin/src/pages/AdminPage/Board/Board.tsx
+++ packages/admin/src/pages/AdminPage/Board/Board.tsx
@@ -1,7 +1,36 @@
+import { useMemo, useState } from "react";
+import { useQuery } from "react-query";
+
+import { Board as BoardType } from "@shared/swagger-api/generated/models/Board";
+import { BoardApiService } from "@shared/swagger-api/generated/services/BoardApiService";
+import { PropsType } from "src/types/utils";
+
 import BoardView from "./Board.view";
 
 const Board = () => {
-  return <BoardView />;
+  const { data } = useQuery([ "boards" ], () =>
+    BoardApiService.boardControllerFind({ uuid: undefined }),
+  );
+
+  const [ selectedBoardStack, setSelectedBoardStack ] = useState<BoardType[]>([]);
+
+  const displayBoards = useMemo(() => {
+    if (selectedBoardStack.length === 0) return data ?? [];
+
+    return selectedBoardStack.at(-1)?.children ?? [];
+  }, [ data, selectedBoardStack ]);
+
+  if (!data) return null;
+
+  const viewProp: PropsType<typeof BoardView> = {
+    boards: displayBoards,
+
+    onClick: (board) => {
+      setSelectedBoardStack([ ...selectedBoardStack, board ]);
+    },
+  };
+
+  return <BoardView {...viewProp} />;
 };
 
 export default Board;
--- packages/admin/src/pages/AdminPage/Board/Board.view.tsx
+++ packages/admin/src/pages/AdminPage/Board/Board.view.tsx
@@ -1,5 +1,30 @@
-const BoardView = () => {
-  return <>board view</>;
-};
+import { Board } from "@shared/swagger-api/generated/models/Board";
+import classnames from "classnames";
 
-export default BoardView;
+import $ from "./Board.module.scss";
+
+interface Props {
+  boards: Board[];
+  onClick?: (board: Board) => any;
+}
+
+export default function BoardView({ boards, onClick }: Props) {
+  return (
+    <div className={$.table}>
+      <div className={classnames($.row, $.header)}>
+        <div className={classnames($.cell, $.id)}>ID</div>
+        <div className={classnames($.cell, $.title)}>이름</div>
+        <div className={classnames($.cell, $.scraps)}>URL</div>
+      </div>
+      {boards.map((board) => {
+        return (
+          <div className={$.row} onClick={() => onClick?.(board)}>
+            <div className={classnames($.cell, $.id)}>{board.id}</div>
+            <div className={classnames($.cell, $.title)}>{board.name}</div>
+            <div className={classnames($.cell, $.scraps)}>{board.url}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}

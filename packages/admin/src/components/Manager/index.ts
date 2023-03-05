6fa9c5f8c3648b6307b868613147c22330951d4c
jess <jaryapp@gmail.com>
2023-03-05 19:00:00 +0900

    🔥 불필요한 폴더 제거

--- packages/admin/src/components/Manager/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ScraperManager } from "./ScraperManager";
-export { default as ScenarioQueue } from "./ScenarioQueue";
-export { default as ExcutionLog } from "./ExcutionLog";
--- packages/admin/src/components/Scenario/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ScenarioCard } from "./ScenarioCard";
-export { default as ScenarioFilter } from "./ScenarioFilter";
-export { default as ScenarioGroupList } from "./ScenarioGroupList";

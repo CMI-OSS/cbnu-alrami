fa9ad982c2c5b5c8b49c4f3a7136aa38f559577a
thdwlsgus0 <thdwlsgus0@naver.com>
2023-03-05 16:51:24 +0900

    refactor: 더미 데이터 제거

--- packages/mobile/src/__mocks__/index.ts
+++ packages/mobile/src/__mocks__/index.ts
@@ -1,15 +1,9 @@
 import cafeteriaMenu from "./cafeteriaMenu";
-import constructionInfo from "./constructionInfo";
 import convenientList from "./convenientList";
-import detailImageList from "./detailImageList";
-import mapImageList from "./mapImageList";
 import menuList from "./menuList";
 
 export {
   menuList,
-  detailImageList,
-  mapImageList,
   cafeteriaMenu,
-  constructionInfo,
   convenientList,
 };

16be32ba36243d0c9840d82068255e9457675d24
동동이 <drdragon777@naver.com>
2023-03-12 22:25:54 +0900

    chore: add roundBox prop

--- packages/mobile/src/components/atoms/SuspenseFallback/index.tsx
+++ packages/mobile/src/components/atoms/SuspenseFallback/index.tsx
@@ -1,16 +1,24 @@
+import classnames from "classnames";
+
 import DeferredComponent from "../DeferredComponent";
 import Loading from "../Loading";
 import $ from "./style.module.scss";
 
 type Props = {
   height: string;
+  isRoundBox?: boolean;
 };
 
 function SuspenseFallback(props: Props) {
-  const { height } = props;
+  const { height, isRoundBox } = props;
   return (
     <DeferredComponent>
-      <div className={$["suspense-fallback"]} style={{ height }}>
+      <div
+        className={classnames($["suspense-fallback"], {
+          [$["round-box"]]: isRoundBox,
+        })}
+        style={{ height }}
+      >
         <Loading width={64} borderWidth={4} color="#D66D6E" />
       </div>
     </DeferredComponent>

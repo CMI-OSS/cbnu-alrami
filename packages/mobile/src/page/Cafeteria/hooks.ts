637b7ffbbfacf079b08165958a9b036f119935d7
동동이 <drdragon777@naver.com>
2023-03-16 00:12:05 +0900

    feat: add get date, menu state from query string

---
+++ packages/mobile/src/page/Cafeteria/hooks.ts
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router-dom";
+
+import { CafeteriaMenu } from "@shared/swagger-api/generated";
+import useSearch from "src/hooks/useSearch";
+import { useAppDispatch, useAppSelector } from "src/store";
+import { selectMenu } from "src/store/cafeteriaSlice";
+
+import caledarReducer, { CalendarAction } from "../Calendar/calendarReducer";
+import { getDateMenuInQueryStr } from "./utils";
+
+export const useCafeteria = () => {
+  const queryDate = useSearch({ target: "date" });
+  const queryMenu = useSearch({ target: "menu" });
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { selectedMenu } = useAppSelector((state) => {
+    return state.persistedReducer.cafeteria.cafeteria;
+  });
+
+  const { year, month, date, day, menu } = getDateMenuInQueryStr(selectedMenu, {
+    queryDate,
+    queryMenu,
+  });
+
+  const getFullDate = (year: number, month: number, date: number) => {
+    return `${year}-${month + 1}-${date}`;
+  };
+
+  const dispatchDate = (type: CalendarAction) => {
+    return caledarReducer({ year, month: month - 1, date, day }, { type });
+  };
+
+  const onDecrease = () => {
+    const { year, month, date } = dispatchDate("decrement_date");
+    navigate(`?date=${getFullDate(year, month, date || 1)}&menu=${menu}`);
+  };
+
+  const onIncrease = () => {
+    const { year, month, date } = dispatchDate("increment_date");
+    navigate(`?date=${getFullDate(year, month, date || 1)}&menu=${menu}`);
+  };
+
+  const handleMenu = (selectedMenu: CafeteriaMenu.name) => {
+    dispatch(selectMenu({ selectedMenu }));
+    const fullDate = getFullDate(year, month - 1, date);
+    navigate(`?date=${fullDate}&menu=${selectedMenu}`);
+  };
+
+  return {
+    year,
+    month: month - 1,
+    date,
+    day,
+    fullDate: getFullDate(year, month - 1, date),
+    selectedMenu: menu,
+    onDecrease,
+    onIncrease,
+    handleMenu,
+  };
+};

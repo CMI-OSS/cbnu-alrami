1cb2fa664189d88b7e061ab02d6b4d5f8d312981
gijin noh <38103082+nohgijin@users.noreply.github.com>
2023-03-16 17:07:14 +0900

    test: 배포 테스트 커밋 (#703)

--- packages/mobile/src/components/shared/ImageModal/index.tsx
+++ packages/mobile/src/components/shared/ImageModal/index.tsx
@@ -16,7 +16,6 @@ type Props = {
 
 function ImageModal({ order, setOrder, onClose, images }: Props) {
   // TODO: 백엔드 수정 후 확인필요
-
   const handleDownloadClick = async () => {
     const { id, url } = images[order];
     const response = await fetch(url);

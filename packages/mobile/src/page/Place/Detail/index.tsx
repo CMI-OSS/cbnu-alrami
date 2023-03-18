de264a35d1b744aa00e2c8a66d9eae30e364cc03
동동이 <drdragon777@naver.com>
2023-03-18 16:15:41 +0900

    feat: apply safe-area in Place

--- packages/mobile/src/page/Place/Detail/index.tsx
+++ packages/mobile/src/page/Place/Detail/index.tsx
@@ -16,12 +16,15 @@ import $ from "./style.module.scss";
 function PlaceDetail() {
   const dispatch = useAppDispatch();
   const position = useSearch({ target: "position" })!;
-  const currentPosition = position === 'all'? undefined : position.split("")[0].toUpperCase();
+  const currentPosition =
+    position === "all" ? undefined : position.split("")[0].toUpperCase();
   const {
     data: schoolData,
     isLoading: schoolLoading,
     isError: schoolError,
-  } = useSchoolQuery({area: currentPosition as PlaceSchoolDto["school"]["area"]});
+  } = useSchoolQuery({
+    area: currentPosition as PlaceSchoolDto["school"]["area"],
+  });
   if (schoolLoading) return <div>로딩중입니다.</div>;
   if (schoolError) return <div>에러가 발생했습니다.</div>;
   if (schoolData === undefined)
@@ -32,7 +35,9 @@ function PlaceDetail() {
   };
 
   const schoolDatas = schoolData.filter((item: PlaceSchoolDto) => {
-    return item?.school.area === currentPosition || currentPosition === undefined;
+    return (
+      item?.school.area === currentPosition || currentPosition === undefined
+    );
   });
 
   const checkMenu = (position: string) => {
@@ -58,16 +63,17 @@ function PlaceDetail() {
           <span className="blind">뒤로가기</span>
         </NavLink>
         <h1 className={$.title}>캠퍼스맵</h1>
+        <ChipGroup
+          list={menuList}
+          handleSelectMenu={handleMenu}
+          selectedMenu={checkMenu(position)}
+        />
         {/* [D] 식당 작업 후 진행 예정 */}
         {/* <NavLink to="/call" className={$.place_link}>
           제보하기
         </NavLink> */}
       </div>
-      <ChipGroup
-        list={menuList}
-        handleSelectMenu={handleMenu}
-        selectedMenu={checkMenu(position)}
-      />
+
       <BorderBox className={$.tooltip} style={{ width: "auto" }}>
         <span className={$.tooltip_title}>
           식사는 현재 베타버전으로, 다양한 맛집이 더 추가될 예정입니다.

a8aa3cf2da59a23eaf84f0d509d8363ccdf49def
HyunoChoi <soonitoon@gmail.com>
2023-03-25 18:08:53 +0900

    chore: Change button colors (#740)

--- packages/mobile/src/page/Calendar/ArrowButton/index.tsx
+++ packages/mobile/src/page/Calendar/ArrowButton/index.tsx
@@ -15,9 +15,9 @@ function ArrowButton({ direction, onClick }: Props) {
       aria-label={`${direction === "left" ? "이전" : "다음"}으로 이동`}
     >
       {direction === "left" ? (
-        <Arrow size={6} stroke="#aaa" />
+        <Arrow size={6} stroke="#5e5e5e" />
       ) : (
-        <Arrow size={6} stroke="#aaa" style={{ transform: "rotate(180deg)" }} />
+        <Arrow size={6} stroke="#5e5e5e" style={{ transform: "rotate(180deg)" }} />
       )}
     </button>
   );

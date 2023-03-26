6394bcbb7d78abf9f2f46e93b68e71dd708b7038
jess <jaryapp@gmail.com>
2023-03-26 15:54:46 +0900

    🔥 deploy shell에 서버만 남기도록 수정

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@ git clean -df
 git reset --hard
 git pull
 yarn
-yarn build:admin
-yarn build:mobile
+# yarn build:admin
+# yarn build:mobile
 yarn build:server
-pm2 stop cbnu_alrami_mobile
-pm2 stop cbnu_alrami_admin
+# pm2 stop cbnu_alrami_mobile
+# pm2 stop cbnu_alrami_admin
 pm2 stop cbnu_alrami_server
-pm2 serve ./packages/admin/dist --port=5001 --spa --name cbnu_alrami_admin
-pm2 serve ./packages/mobile/dist --port=5002 --spa --name cbnu_alrami_mobile
+# pm2 serve ./packages/admin/dist --port=5001 --spa --name cbnu_alrami_admin
+# pm2 serve ./packages/mobile/dist --port=5002 --spa --name cbnu_alrami_mobile
 pm2 start ./packages/server/dist/main.js --name cbnu_alrami_server

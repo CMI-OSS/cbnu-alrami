27d07242d85c1731ad555bb91559eec1b9990129
jess <jaryapp@gmail.com>
2023-04-04 05:03:37 +0900

    ✨ 스크래퍼 스크립트 버그 수정

--- packages/scraper/src/main.ts
+++ packages/scraper/src/main.ts
@@ -1,4 +1,10 @@
 import { apiServer } from "@shared/constant";
 import { OpenAPI } from "@shared/swagger-api/generated/core/OpenAPI";
 
+import { scrapingCafeterias } from "./cafeteria-scraper/cafeteria-scraper";
+import { login } from "./common/login";
+
 OpenAPI.BASE = apiServer.local;
+
+login();
+scrapingCafeterias();
--- packages/scraper/src/notice-scraper/notice-scraper.ts
+++ packages/scraper/src/notice-scraper/notice-scraper.ts
@@ -1,8 +1,10 @@
 import { ArticleApiService } from "@shared/swagger-api/generated/services/ArticleApiService";
 import dayjs from "dayjs";
+import { log } from "src/common/log";
 import noticeScripts from "src/notice-scraper/scripts/index";
 
 import { scraping } from "../scraper/scraper";
+import { excludeNotices, excludeSites } from "./constant";
 
 const getISODate = (date: string) => {
   const replacedDate = date.replace(/[년|일|월]/g, ".");
@@ -12,6 +14,15 @@ const getISODate = (date: string) => {
 
 export const scrapingNotices = async () => {
   for (const noticeScript of noticeScripts) {
+    if (
+      excludeSites.some((site) => {
+        return site.script.site_id === noticeScript.site_id;
+      })
+    ) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+
     // 공지사항 목록 가져오기
     let noticeList = [];
     try {
@@ -29,9 +40,32 @@ export const scrapingNotices = async () => {
       }
     } catch (error) {
       console.error("[ERROR] 공지사항 목록 가져오기 - ", noticeScript, error);
+      log(
+        `[ERROR] 공지사항 목록 가져오기 - ${JSON.stringify({
+          noticeScript,
+          error,
+        })}`,
+      );
     }
     try {
       for (const notice of noticeList) {
+        if (
+          excludeNotices.some((excludeNotice) => {
+            return excludeNotice.url === notice.url;
+          })
+        ) {
+          // eslint-disable-next-line no-continue
+          continue;
+        }
+
+        const duplicateResponse =
+          await ArticleApiService.articleControllerIsDuplicated({
+            url: notice.url,
+          });
+
+        // eslint-disable-next-line no-continue
+        if (duplicateResponse.isDuplicated) continue;
+
         // 공지사항 내용가져오기
         const content = await scraping({
           scenario: {
@@ -42,6 +76,19 @@ export const scrapingNotices = async () => {
             waitSelector: noticeScript.noticeContentsSelector,
           },
         });
+
+        // eslint-disable-next-line no-extra-boolean-cast
+        if (!!(noticeScript as any).getContentDate) {
+          notice.date = await scraping({
+            scenario: {
+              name: noticeScript.site,
+              jsScript: noticeScript,
+              scrapFunctionName: (noticeScript as any).getContentDate.name,
+              url: notice.url,
+              waitSelector: noticeScript.noticeContentsSelector,
+            },
+          });
+        }
 
         try {
           // 공지사항 등록
@@ -59,10 +106,22 @@ export const scrapingNotices = async () => {
             console.log("[INFO] 공지사항 등록 완료 - ", notice);
           }
         } catch (error) {
+          log(
+            `[ERROR] 공지사항 등록 - ${JSON.stringify({
+              notice,
+              error,
+            })}`,
+          );
           console.error("[ERROR] 공지사항 등록 - ", error, notice);
         }
       }
     } catch (error) {
+      log(
+        `[ERROR] 공지사항 등록 - ${JSON.stringify({
+          noticeScript,
+          error,
+        })}`,
+      );
       console.error("[ERROR] 공지사항 등록 - ", error, noticeScript);
     }
   }

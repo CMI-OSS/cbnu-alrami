155bff3629384f2fc37c861f32dad2a23d2c2b84
jess <jaryapp@gmail.com>
2023-04-05 15:14:24 +0900

    ✨ 서버 PM2 메모리 한계 1GB로 설정

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ yarn build:server
 pm2 stop cbnu_alrami_server
 # pm2 serve ./packages/admin/dist --port=5001 --spa --name cbnu_alrami_admin
 # pm2 serve ./packages/mobile/dist --port=5002 --spa --name cbnu_alrami_mobile
-pm2 start ./packages/server/dist/main.js --name cbnu_alrami_server
+pm2 start ./packages/server/dist/main.js --name cbnu_alrami_server --max-memory-restart 1000M

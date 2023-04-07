bfe7747e09984f460103b548244ae12971c98583
동동이 <drdragon777@naver.com>
2023-04-07 17:59:44 +0900

    refactor: add CustomQueryOptions, remove unnecessary type

--- packages/mobile/src/hooks/api/core/index.ts
+++ packages/mobile/src/hooks/api/core/index.ts
@@ -12,13 +12,19 @@ import type {
 import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
 
+export type CustomQueryOptions<T, U> = UseQueryOptions<
+  T,
+  AxiosError,
+  U extends null ? T : U
+> & {
+  queryKey: QueryKey;
+  queryFn: QueryFunction<T, QueryKey>;
+};
+
 export function useCoreQuery<T, U = null>(
   keyName: QueryKey,
   query: QueryFunction<T, QueryKey>,
-  options?: Omit<
-    UseQueryOptions<T, AxiosError, U extends null ? T : U>,
-    "queryKey" | "queryFn"
-  >,
+  options?: Omit<CustomQueryOptions<T, U>, "queryKey" | "queryFn">,
 ): UseQueryResult<U extends null ? T : U, AxiosError> {
   return useQuery(keyName, query, {
     onError: (err) => {
@@ -30,7 +36,7 @@ export function useCoreQuery<T, U = null>(
 
 export function useCoreMutation<T, U>(
   mutation: MutationFunction<T, U>,
-  options?: Omit<UseMutationOptions<T, AxiosError, U>, "mutationKey">,
+  options?: UseMutationOptions<T, AxiosError, U>,
 ): UseMutationResult<T, AxiosError, U> {
   return useMutation(mutation, {
     onError: (err) => {
@@ -43,10 +49,7 @@ export function useCoreMutation<T, U>(
 export function useCoreInfiniteQuery<T>(
   keyName: QueryKey,
   query: QueryFunction<T, QueryKey>,
-  options?: Omit<
-    UseInfiniteQueryOptions<T, AxiosError>,
-    "queryKey" | "queryFn"
-  >,
+  options?: UseInfiniteQueryOptions<T, AxiosError>,
 ): UseInfiniteQueryResult<T, AxiosError> {
   return useInfiniteQuery(keyName, query, {
     onError: (err) => {

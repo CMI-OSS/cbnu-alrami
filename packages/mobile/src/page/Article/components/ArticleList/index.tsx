b101841d8602cf7102c44a73b40ed56556e08cdb
gijin noh <38103082+nohgijin@users.noreply.github.com>
2023-04-07 20:05:10 +0900

    [mobile] 무한스크롤 root margin 수정 (#861)
    * fix: 무한스크롤 root margin 수정
    
    * chore: 태그 하나로 사용

--- packages/mobile/src/page/Article/components/ArticleList/index.tsx
+++ packages/mobile/src/page/Article/components/ArticleList/index.tsx
@@ -43,12 +43,15 @@ function ArticleList({ className }: DefaultProps) {
     fetchNextPage,
   } = useArticles({ kind });
 
-  const ref = useIntersect(async (entry, observer) => {
-    observer.unobserve(entry.target);
-    if (hasNextPage && !isFetching) {
-      await fetchNextPage();
-    }
-  });
+  const ref = useIntersect(
+    async (entry, observer) => {
+      observer.unobserve(entry.target);
+      if (hasNextPage && !isFetching) {
+        await fetchNextPage();
+      }
+    },
+    { rootMargin: "0px 0px 30% 0px" },
+  );
 
   const isArticleEmpty =
     !articlesData?.pages[0]?.articles ||

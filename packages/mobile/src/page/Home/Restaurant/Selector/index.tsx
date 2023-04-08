a681966832dc1cc9beeca82061fa5630294ee4e1
Hyuno Choi <soonitoon@gmail.com>
2023-04-08 13:15:35 +0900

    feat: Show comfirm modal when user select "표시안함" option

--- packages/mobile/src/page/Home/Restaurant/Selector/index.tsx
+++ packages/mobile/src/page/Home/Restaurant/Selector/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import classNames from "classnames";
 import BorderBox from "src/components/atoms/BorderBox";
 import { CAFETERIA_LIST } from "src/consts";
@@ -23,6 +25,24 @@ function Selector({
   onCafeteriaSelect,
   className,
 }: Props) {
+  const [ isConfirmOpen, setIsConfirmOpen ] = useState(false);
+
+  const handleSelect = (name: Restaurant) => {
+    if (name === "표시 안함") {
+      setIsConfirmOpen(true);
+      return;
+    }
+    onCafeteriaSelect(name);
+  };
+
+  const handleCloseConfirmModal = () => {
+    return setIsConfirmOpen(false);
+  };
+
+  const handleAgree = () => {
+    return onCafeteriaSelect("표시 안함");
+  };
+
   return (
     <div className={$["dimmed-box"]}>
       <BorderBox
@@ -41,7 +61,7 @@ function Selector({
                     [$.selected]: name === cafeteriaName,
                   })}
                   onClick={() => {
-                    return onCafeteriaSelect(name as Restaurant);
+                    return handleSelect(name as Restaurant);
                   }}
                   aria-label={`${name} 선택하기`}
                 >
@@ -55,6 +75,25 @@ function Selector({
           취소
         </button>
       </BorderBox>
+      {isConfirmOpen && (
+        <div className={$["confirm-dimmed-box"]}>
+          <div className={$["confirm-modal"]}>
+            <span className={$.description}>
+              홈화면에 식단을 표시하지
+              <br />
+              않으시겠습니까?
+            </span>
+            <div className={$["button-box"]}>
+              <button type="button" aria-label="선택 취소하기" className={$.button} onClick={handleCloseConfirmModal}>
+                취소
+              </button>
+              <button type="button" aria-label="식단 표시하지 않기" className={$.button} onClick={handleAgree}>
+                표시안함
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

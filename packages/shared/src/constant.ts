f2d249debf6a226b4c37fa7e7fcfb80a0e5a415d
jess <jaryapp@gmail.com>
2023-04-09 18:19:46 +0900

    ✨ 스크래퍼 구분

--- packages/shared/src/constant.ts
+++ packages/shared/src/constant.ts
@@ -1,5 +1,6 @@
 export const apiServer = {
   local: "http://localhost:3001",
+  production_local: "http://localhost:3002",
   dev: "https://dev-server.cmiteam.kr",
   production: "https://server.cmiteam.kr",
 };

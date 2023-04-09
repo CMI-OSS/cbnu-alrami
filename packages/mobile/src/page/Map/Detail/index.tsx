af486fe9322f8827b1f7b2d47906e13eae52b855
thdwlsgus0 <thdwlsgus0@naver.com>
2023-04-09 18:10:51 +0900

    style: 캠퍼스맵 상세 조회 헤더 스타일 수정

--- packages/mobile/src/page/Map/Detail/index.tsx
+++ packages/mobile/src/page/Map/Detail/index.tsx
@@ -8,20 +8,23 @@ import MapDetailBody from "src/page/Map/Detail/MapDetailBody";
 function MapDetail() {
   const bodyHeight =
     "calc(var(--vh, 1vh) * 100 - (env(safe-area-inset-top) + env(safe-area-inset-bottom)))";
-  
+
   return (
     <FullPageModalTemplate
-      left={<LeftArrow stroke="#5e5e5e" size={16} />}
-      style={{ background: "none" }}
+      left={<LeftArrow stroke="#fff" size={16} />}
+      style={{
+        background:
+          "linear-gradient(180deg, rgba(0, 0, 0, 0.5) 22.92%, rgba(0, 0, 0, 0) 100%)",
+      }}
     >
       {/* [D] 식당 스펙 추가 후 작업 진행 예정 */}
       {/* <NavLink to="/call" className={$["link-call"]}>
           <span className="blind">제보하기</span>
         </NavLink> */}
       <AsyncBoundary
-          suspenseFallback={<SuspenseFallback height={bodyHeight} />}
-          errorFallback={ErrorFallback}
-          fallBackHeight={bodyHeight}
+        suspenseFallback={<SuspenseFallback height={bodyHeight} />}
+        errorFallback={ErrorFallback}
+        fallBackHeight={bodyHeight}
       >
         <MapDetailBody />
       </AsyncBoundary>

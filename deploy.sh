00c476bd77ddbea621f9803b9267e4aaf230fee7
jess <jaryapp@gmail.com>
2023-04-09 17:27:13 +0900

    ✨ 스크래퍼 배포 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ pm2 stop all
 # pm2 serve ./packages/admin/dist --port=5001 --spa --name cbnu_alrami_admin
 # pm2 serve ./packages/mobile/dist --port=5002 --spa --name cbnu_alrami_mobile
 pm2 start ./packages/server/dist/main.js --name cbnu_alrami_server
-pm2 start yarn --name cbnu_alrami_scraper -- start
+pm2 start yarn --name cbnu_alrami_scraper -- start:scraper

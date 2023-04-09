5638e01fb14a86fb57605c68167f38e154622814
jess <jaryapp@gmail.com>
2023-04-10 02:10:10 +0900

    📝 `deploy` 쉘 수정

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ yarn
 yarn build:server
 pm2 stop cbnu_alrami_server
 pm2 stop cbnu_alrami_scraper
+pm2 delete cbnu_alrami_server
+pm2 delete cbnu_alrami_scraper
 # pm2 serve ./packages/admin/dist --port=5001 --spa --name cbnu_alrami_admin
 # pm2 serve ./packages/mobile/dist --port=5002 --spa --name cbnu_alrami_mobile
 pm2 start ./packages/server/dist/main.js --name cbnu_alrami_server

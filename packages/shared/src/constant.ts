800c26ea58cd806cc5018a0fcb2fcf4abc0f107f
jess <jaryapp@gmail.com>
2023-04-09 18:07:24 +0900

    ✨ production 서버 연결

--- packages/shared/src/constant.ts
+++ packages/shared/src/constant.ts
@@ -1,6 +1,5 @@
 export const apiServer = {
   local: "http://localhost:3001",
   dev: "https://dev-server.cmiteam.kr",
-  // TODO: 배포되면 바꿔야함
-  production: "https://dev-server.cmiteam.kr",
+  production: "https://server.cmiteam.kr",
 };

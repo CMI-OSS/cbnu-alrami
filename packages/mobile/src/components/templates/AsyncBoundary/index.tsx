b07ef02e26687f65525fff96011d2c779df1c692
동동이 <drdragon777@naver.com>
2023-04-16 01:01:34 +0900

    refactor: remove style prop in boundary components, test code

--- packages/mobile/src/components/templates/AsyncBoundary/index.tsx
+++ packages/mobile/src/components/templates/AsyncBoundary/index.tsx
@@ -2,28 +2,23 @@ import { PropsWithChildren, ReactElement, Suspense, useCallback } from "react";
 
 import { useQueryErrorResetBoundary } from "@tanstack/react-query";
 
-import ErrorBoundary from "../ErrorBoundary";
+import ErrorBoundary, { ErrorBoundaryProps } from "../ErrorBoundary";
 
 type Props = {
   suspenseFallback: ReactElement;
-  errorFallback: (...args: any[]) => ReactElement;
-  fallBackHeight?: string;
-  keys?: Array<unknown>;
+  errorFallback: ErrorBoundaryProps["errorFallback"];
+  keys?: ErrorBoundaryProps["keys"];
 };
 
 const AsyncBoundary = (props: PropsWithChildren<Props>) => {
-  const { suspenseFallback, errorFallback, children, keys, fallBackHeight } =
-    props;
+  const { suspenseFallback, errorFallback, children, keys } = props;
   const { reset } = useQueryErrorResetBoundary();
   const resetHandler = useCallback(() => {
     reset();
   }, [ reset ]);
 
   return (
-    <ErrorBoundary
-      resetQuery={resetHandler}
-      {...{ errorFallback, fallBackHeight, keys }}
-    >
+    <ErrorBoundary resetQuery={resetHandler} {...{ errorFallback, keys }}>
       <Suspense fallback={suspenseFallback}>{children}</Suspense>
     </ErrorBoundary>
   );

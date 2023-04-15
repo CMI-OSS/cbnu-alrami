2b00d652405f7a90683dd78e843d4f0714df6ef0
동동이 <drdragon777@naver.com>
2023-04-16 01:02:27 +0900

    refactor: add type guards in fallback components

--- packages/mobile/src/components/atoms/ErrorFallback/index.tsx
+++ packages/mobile/src/components/atoms/ErrorFallback/index.tsx
@@ -1,19 +1,37 @@
+import classnames from "classnames";
+import { StyleProps } from "src/type/props";
+
 import $ from "./style.module.scss";
 
-type Props = {
-  error: Error;
+export type ErrorFallbackProps = {
+  error: Error | null;
   reset: () => void;
-  height: string;
-};
+  height?: string;
+} & StyleProps;
 
-function ErrorFallback(props: Props) {
-  const { error, reset, height } = props;
+function ErrorFallback(props: ErrorFallbackProps) {
+  const { error, reset, height, className, style } = props;
+
+  if (!height && !style && !className) {
+    throw new Error(
+      "Either 'height' or 'style' or 'className' prop must be provided in ErrorFallback component ",
+    );
+  }
+  if (!error) throw new Error("error does not exist");
+
+  const errorMsg = error.message
+    ? error.message
+    : "알 수 없는 오류가 발생했습니다.";
+
   return (
-    <div className={$["error-fallback"]} style={{ height }}>
+    <div
+      className={classnames($["error-fallback"], className)}
+      style={{ ...style, height }}
+    >
       <div className={$["error-fallback-box"]}>
         <span
           className={$["error-msg"]}
-          dangerouslySetInnerHTML={{ __html: error?.message }}
+          dangerouslySetInnerHTML={{ __html: errorMsg }}
         />
         <button type="button" onClick={reset} className={$["reset-btn"]}>
           다시 시도

90d87286763ddc83412be6d31af013743a2700ec
jess <jaryapp@gmail.com>
2023-04-16 17:07:36 +0900

    🚧 deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
-git clean -df
-git reset --hard
-git pull
-yarn
-# yarn build:admin
-# yarn build:mobile
-yarn build:server
-pm2 stop cbnu_alrami_server
-pm2 stop cbnu_alrami_scraper
-pm2 delete cbnu_alrami_server
-pm2 delete cbnu_alrami_scraper
-# pm2 serve ./packages/admin/dist --port=5001 --spa --name cbnu_alrami_admin
-# pm2 serve ./packages/mobile/dist --port=5002 --spa --name cbnu_alrami_mobile
-pm2 start ./packages/server/dist/main.js --name cbnu_alrami_server
-pm2 start yarn --name cbnu_alrami_scraper -- start:scraper
+
+pm2 stop cbnu_alrami_scraper_test
+pm2 delete cbnu_alrami_scraper_test
+pm2 start yarn --name cbnu_alrami_scraper_test -- start:scraper

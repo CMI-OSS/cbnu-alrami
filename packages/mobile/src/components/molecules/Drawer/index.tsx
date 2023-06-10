1813588925fa56a8410334fd58908369584b2026
nohgijin <ynda11111@naver.com>
2023-06-10 12:33:39 +0900

    feat: toggle 컴포넌트 생성

--- packages/mobile/src/components/molecules/Drawer/index.tsx
+++ packages/mobile/src/components/molecules/Drawer/index.tsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
+
 import { Alarm, Subscription } from "@components/atoms/icon";
 import Icon from "@components/atoms/icon/Icon";
+import Toggle from "@components/atoms/Toggle";
 
 import $ from "./style.module.scss";
 
 function Drawer() {
+  const [ checked, setChecked ] = useState(false);
+
   return (
     <div className={$.drawer}>
       <div className={$.item}>
         <Subscription size={20} />
         구독
+        <Toggle
+          checked={checked}
+          onClick={() => {
+            return setChecked(!checked);
+          }}
+        />
       </div>
       <div className={$.item}>
         <Alarm size={20} />
         알람
+        <Toggle
+          checked
+          onClick={() => {
+            return console.log("알림");
+          }}
+        />
       </div>
       <div className={$.item}>
         <Icon name="view" size={20} color="#828282" />

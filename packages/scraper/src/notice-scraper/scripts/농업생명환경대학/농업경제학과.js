8740c68db729cd9659b469b03614b15055323327
jess <jaryapp@gmail.com>
2023-07-07 03:52:55 +0900

    📝 농업 경제학과 스크립트 수정

--- packages/scraper/src/notice-scraper/scripts/농업생명환경대학/농업경제학과.js
+++ packages/scraper/src/notice-scraper/scripts/농업생명환경대학/농업경제학과.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-inner-declarations */
 import { boardTree } from "@shared/board-tree/board-tree.generated";
 
 const script = {
@@ -11,12 +12,30 @@ const script = {
     const list = document.querySelectorAll(this.noticeListSelector);
     return Array.from(list).map((item) => {
       const td = item.querySelectorAll("td");
+      const urlTag = td[1].querySelector("a");
+      let url = urlTag.href.trim();
+
+      if (urlTag.getAttribute("onclick")) {
+        function parseId(content) {
+          const regex = /(\d+)/g;
+          const matches = content.match(regex);
+          if (matches && matches.length > 1) {
+            return matches[1];
+          }
+          return null;
+        }
+
+        const id = parseId(urlTag.getAttribute("onclick"));
+
+        if (id) url = `${script.url}&mod=view&pidx=${id}&page=1`;
+      }
+
       return {
         site: this.site,
         category: this.category,
         site_id: this.site_id,
         title: td[1].querySelector("a").innerText.trim(),
-        url: td[1].querySelector("a").href.trim(),
+        url,
         date:
           this.site === "농업경제학과"
             ? td[2].innerText.trim()

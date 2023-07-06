5312b4d5cab316e872929a8ac008e717878261f6
jess <jaryapp@gmail.com>
2023-07-07 03:02:53 +0900

    ✨ cron 정책 변경

--- packages/scraper/src/main.ts
+++ packages/scraper/src/main.ts
@@ -4,14 +4,14 @@ import { scrapingCafeterias } from "./cafeteria-scraper/cafeteria-scraper";
 import { log } from "./common/log";
 import { scrapingNotices } from "./notice-scraper/notice-scraper";
 
-// 3시간에 한번씩 실행
-schedule("0 */3 * * *", () => {
+// 매일 오전 12시에 한번씩 실행
+schedule("0 0 * * *", () => {
   log(`[INFO] Start scrapingCafeterias`);
   scrapingCafeterias();
 });
 
-// 매일 오전 6시부터 오후 11시까지 30분 마다 한번씩 실행
-schedule("*/30 6-23 * * *", () => {
+// 평일 오전 9시부터 오후 9시까지 3시간 마다 한번씩 실행
+schedule("0 9-21/3 * * 1-5", () => {
   log(`[INFO] Start scrapingNotices`);
   scrapingNotices();
 });

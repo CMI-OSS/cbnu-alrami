216747032cecec3966a334c3a0aa39c8af2dd122
jess <jaryapp@gmail.com>
2023-08-21 00:38:53 +0900

    ✨ 기계공학부 스크립트 수정

--- packages/scraper/src/notice-scraper/scripts/공과대학/기계공학부.js
+++ packages/scraper/src/notice-scraper/scripts/공과대학/기계공학부.js
@@ -1,13 +1,12 @@
 import { boardTree } from "@shared/board-tree/board-tree.generated";
 
 const script = {
-  url: "https://me.chungbuk.ac.kr/index.php?mid=me_sub04",
+  url: "https://me.chungbuk.ac.kr/me5_1",
   site_id: boardTree.전공.공과대학.기계공학부.공지사항.id,
   site: "기계공학부",
   category: "공지사항",
-  noticeListSelector: "#bd_839_0 > div.bd_lst_wrp > table > tbody > tr",
-  noticeContentsSelector:
-    "#content > div.bd.hover_effect > div.rd.rd_nav_style2.clear > div.rd_body.clear > article",
+  noticeListSelector: "#bd_172_0 > div.bd_lst_wrp > table > tbody > tr",
+  noticeContentsSelector: "div.rd_body > article",
   getNoticeList() {
     const list = document.querySelectorAll(this.noticeListSelector);
     return Array.from(list).map((item) => {
@@ -16,9 +15,9 @@ const script = {
         site: this.site,
         category: this.category,
         site_id: this.site_id,
-        title: td[2].innerText.trim(),
-        url: td[2].querySelector("a").href.trim(),
-        date: td[4].innerText.trim(),
+        title: td[1].innerText.trim(),
+        url: td[1].querySelector("a").href.trim(),
+        date: td[3].innerText.trim(),
       };
     });
   },
